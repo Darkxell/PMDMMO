af2641a5b2bbcfeba34531a20e548c4dae2fd804
CANDELA Nicolas <darkxell.mc@gmail.com>
2017-08-22 15:56:36 +0200

    Added project files

---
+++ src/com/darkxell/client/launchable/Launcher.java
@@ -0,0 +1,11 @@
+package com.darkxell.client.launchable;
+
+/**Launching class of the client*/
+public class Launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
