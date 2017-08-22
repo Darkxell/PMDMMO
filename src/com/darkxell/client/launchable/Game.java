1baf31c118a12ad8ebe7435760ff7379a5d5f373
Cubiccl <ccl.clement@hotmail.fr>
2017-08-22 18:19:36 +0200

    Abstract state & StateManager
    With a test state and transitions.

--- src/com/darkxell/client/launchable/Game.java
+++ src/com/darkxell/client/launchable/Game.java
@@ -3,11 +3,15 @@ package com.darkxell.client.launchable;
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 
+import com.darkxell.client.state.StateManager;
+import com.darkxell.client.state.TestState;
+
 public class Game implements Runnable
 {
 
 	/** Set to false to stop the game. */
 	private boolean isRunning;
+	public StateManager stateManager;
 
 	public Game()
 	{}
@@ -17,11 +21,10 @@ public class Game implements Runnable
 	{
 		BufferStrategy bf = Launcher.frame.canvas.getBufferStrategy();
 		Graphics2D g = (Graphics2D) bf.getDrawGraphics();
-		int width = Launcher.frame.getWidth(), height = Launcher.frame.getHeight();
+		int width = Launcher.frame.canvas.getWidth(), height = Launcher.frame.canvas.getHeight();
 		g.clearRect(0, 0, width, height);
 
-		// Draw here
-		g.drawString("System.out.println(\"Hello world\");", 1, g.getFont().getSize());
+		this.stateManager.render(g, width, height);
 
 		g.dispose();
 		bf.show();
@@ -31,6 +34,8 @@ public class Game implements Runnable
 	public void run()
 	{
 		this.isRunning = true;
+		this.stateManager = new StateManager();
+		this.stateManager.setState(new TestState(), 0);
 
 		while (this.isRunning)
 		{
@@ -56,7 +61,7 @@ public class Game implements Runnable
 	/** Updates the game. */
 	private void update()
 	{
-		// TODO Auto-generated method stub
+		this.stateManager.update();
 	}
 
 }
