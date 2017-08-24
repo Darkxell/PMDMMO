c9a7068a8dac40c97d4fa670174aa9d4cdb60e96
CANDELA Nicolas <darkxell.mc@gmail.com>
2017-08-24 03:19:20 +0200

    Added assets folder for work purposes

---
+++ assets/sans-titre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="square" tilewidth="8" tileheight="8" tilecount="10591" columns="119">
+ <image source="square.png" width="958" height="719"/>
+</tileset>
---
+++ assets/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="terrain" tilewidth="8" tileheight="8" tilecount="2" columns="2">
+ <image source="terrain.png" width="16" height="8"/>
+</tileset>
