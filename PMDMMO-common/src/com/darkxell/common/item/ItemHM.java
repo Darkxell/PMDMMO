40a5bea5e4ebc33ee97ce513500e789f634fd12c
Cubiccl <ccl.clement@hotmail.fr>
2017-08-26 11:52:40 +0200

    TM & HMs

---
+++ PMDMMO-common/src/com/darkxell/common/item/ItemHM.java
@@ -0,0 +1,30 @@
+package com.darkxell.common.item;
+
+import org.jdom2.Element;
+
+/** An Item that teaches a move to a Pokémon when used. */
+public class ItemHM extends Item
+{
+
+	/** The move this TM teaches. */
+	public final int moveID;
+
+	public ItemHM(Element xml)
+	{
+		super(xml);
+		this.moveID = Integer.parseInt(xml.getAttributeValue("move"));
+	}
+
+	public ItemHM(int id, int price, int sell, int moveID)
+	{
+		super(id, price, sell);
+		this.moveID = moveID;
+	}
+
+	@Override
+	public Element toXML()
+	{
+		return super.toXML().setAttribute("move", Integer.toString(this.moveID));
+	}
+
+}
