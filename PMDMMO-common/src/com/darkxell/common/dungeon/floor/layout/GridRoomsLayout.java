05bd1d842c900524fc3fba898412406977e8267b
Cubiccl <ccl.clement@hotmail.fr>
2017-08-27 17:36:12 +0200

    Almost working but found better method

--- PMDMMO-common/src/com/darkxell/common/dungeon/floor/layout/GridRoomsLayout.java
+++ PMDMMO-common/src/com/darkxell/common/dungeon/floor/layout/GridRoomsLayout.java
@@ -43,6 +43,57 @@ public class GridRoomsLayout extends Layout
 		this.maxRoomHeight = maxRoomHeight;
 	}
 
+	/** @return The possible coordinates for the topleft corner of the input room. */
+	private Rectangle availableSpace(Rectangle r, Rectangle room)
+	{
+		boolean improved = true;
+		Rectangle test = null;
+		while (improved)
+		{
+			improved = false;
+
+			// Move left
+			test = (Rectangle) r.clone();
+			--test.x;
+			if (this.isValid(test))
+			{
+				improved = true;
+				r = (Rectangle) test.clone();
+			}
+
+			// Move up
+			test = (Rectangle) r.clone();
+			--test.y;
+			if (this.isValid(test))
+			{
+				improved = true;
+				r = (Rectangle) test.clone();
+			}
+
+			// Increase width
+			test = (Rectangle) r.clone();
+			++test.width;
+			if (this.isValid(test))
+			{
+				improved = true;
+				r = (Rectangle) test.clone();
+			}
+
+			// Increase height
+			test = (Rectangle) r.clone();
+			++test.height;
+			if (this.isValid(test))
+			{
+				improved = true;
+				r = (Rectangle) test.clone();
+			}
+
+		}
+		r.width -= room.width;
+		r.height -= room.height;
+		return r;
+	}
+
 	private void createGrid()
 	{
 		// TODO better grid creation
@@ -72,7 +123,6 @@ public class GridRoomsLayout extends Layout
 	@Override
 	protected void generateRooms()
 	{
-		System.out.println(this.rooms.length);
 		this.createGrid();
 
 		// Create random sizes
@@ -102,7 +152,6 @@ public class GridRoomsLayout extends Layout
 
 					for (int row = 0; row < y; ++row)
 						this.rects[x][y].y += this.heights[row] + MIN_SPACE;
-					System.out.println(this.rects[x][y]);
 				}
 
 		// Erode rooms
@@ -134,11 +183,22 @@ public class GridRoomsLayout extends Layout
 		}
 
 		// Add potential additional space
+		ArrayList<Rectangle> rooms = this.rooms();
+		while (!rooms.isEmpty())
+		{
+			Rectangle room = rooms.get(this.random.nextInt(rooms.size()));
+			if (room != null)
+			{
+				Rectangle r = this.availableSpace((Rectangle) room.clone(), room);
+				room.x = this.random.nextInt((int) (r.getMaxX() - r.x + 1)) + r.x;
+				room.y = this.random.nextInt((int) (r.getMaxY() - r.y + 1)) + r.y;
+			}
+			rooms.remove(room);
+		}
 
 		// Center rooms
 		int xStart = Floor.ALL_WIDTH / 2 - this.totalWidth() / 2;
 		int yStart = Floor.ALL_HEIGHT / 2 - this.totalHeight() / 2;
-		System.out.println(xStart + "," + yStart);
 
 		for (int x = 0; x < this.rects.length; ++x)
 			for (int y = 0; y < this.rects[x].length; ++y)
@@ -148,31 +208,36 @@ public class GridRoomsLayout extends Layout
 					this.rects[x][y].y += yStart;
 				}
 
-		/* this.horizSpace = this.random.nextInt(MAX_SPACE - MIN_SPACE + 1) + MIN_SPACE; this.vertiSpace = this.random.nextInt(MAX_SPACE - MIN_SPACE + 1) + MIN_SPACE; int totalWidth = this.sizes.length * this.dimensions.width + (this.sizes.length - 1) * this.horizSpace; int totalHeight =
-		 * this.sizes[0].length * this.dimensions.height + (this.sizes[0].length - 1) * this.vertiSpace;
-		 * 
-		 * while (totalWidth >= Floor.MAX_WIDTH - 2) { Dimension d = this.randomXReduceable(); if (d == null) { --this.horizSpace; totalWidth -= this.sizes.length; } else { --d.width; --totalWidth; } } while (totalHeight >= Floor.MAX_HEIGHT - 2) { Dimension d = this.randomYReduceable(); if (d ==
-		 * null) { --this.vertiSpace; totalHeight -= this.sizes[0].length; } else { --d.height; --totalHeight; } }
-		 * 
-		 * int x = Floor.MAX_WIDTH / 2 - totalWidth / 2 + Floor.UNBREAKABLE + 1; int startY = Floor.MAX_HEIGHT / 2 - totalHeight / 2 + Floor.UNBREAKABLE + 1; */
 		int roomID = 0;
-
-		for (int i = 0; i < this.rects.length; ++i)
-		{
-			for (int j = 0; j < this.rects[0].length; ++j)
+		for (int x = 0; x < this.rects.length; ++x)
+			for (int y = 0; y < this.rects[x].length; ++y)
 			{
-				Rectangle r = this.rects[i][j];
+				Rectangle r = this.rects[x][y];
 				if (r != null)
 				{
 					this.rooms[roomID] = new Room(this.floor, r.x, r.y, r.width, r.height, false);
 					++roomID;
 				}
 			}
-		}
 
 		this.rects = null;
 		this.widths = null;
 		this.heights = null;
+	}
+
+	/** @param except - These rooms will be excluded in the test.
+	 * @return true if the input test room doesn't collide with any other room. */
+	private boolean isValid(Rectangle test, Rectangle... except)
+	{
+		if (Floor.WALKABLE.x >= test.x || Floor.WALKABLE.y >= test.y || Floor.WALKABLE.getMaxX() <= test.getMaxX()
+				|| Floor.WALKABLE.getMaxY() <= test.getMaxY()) return false;
+
+		ArrayList<Rectangle> r = this.rooms();
+		for (Rectangle rectangle : except)
+			r.remove(rectangle);
+		for (Rectangle rectangle : r)
+			if (rectangle.intersects(test)) return false;
+		return true;
 	}
 
 	/** @return A Random location for a Room. */
@@ -202,10 +267,7 @@ public class GridRoomsLayout extends Layout
 	/** @return A Random room which width can be reduced. */
 	private Rectangle randomXReduceable()
 	{
-		ArrayList<Rectangle> candidates = new ArrayList<Rectangle>();
-		for (int x = 0; x < this.rects.length; ++x)
-			for (int y = 0; y < this.rects[x].length; ++y)
-				candidates.add(this.rects[x][y]);
+		ArrayList<Rectangle> candidates = this.rooms();
 
 		candidates.removeIf(new Predicate<Rectangle>()
 		{
@@ -222,10 +284,7 @@ public class GridRoomsLayout extends Layout
 	/** @return A Random room which height can be reduced. */
 	private Rectangle randomYReduceable()
 	{
-		ArrayList<Rectangle> candidates = new ArrayList<Rectangle>();
-		for (int x = 0; x < this.rects.length; ++x)
-			for (int y = 0; y < this.rects[x].length; ++y)
-				candidates.add(this.rects[x][y]);
+		ArrayList<Rectangle> candidates = this.rooms();
 
 		candidates.removeIf(new Predicate<Rectangle>()
 		{
@@ -237,6 +296,16 @@ public class GridRoomsLayout extends Layout
 		});
 		if (candidates.size() == 0) return null;
 		return candidates.get(this.random.nextInt(candidates.size()));
+	}
+
+	private ArrayList<Rectangle> rooms()
+	{
+		ArrayList<Rectangle> rooms = new ArrayList<Rectangle>();
+		for (int x = 0; x < this.rects.length; ++x)
+			for (int y = 0; y < this.rects[x].length; ++y)
+				rooms.add(this.rects[x][y]);
+		rooms.remove(null);
+		return rooms;
 	}
 
 	private int totalHeight()
