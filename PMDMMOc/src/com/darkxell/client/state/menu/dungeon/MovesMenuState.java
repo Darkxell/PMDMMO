e9972da910fdae535b403bc92fd6ec6a96b65fbe
Cubiccl <ccl.clement@hotmail.fr>
2017-09-14 17:54:17 +0200

    Move ordering

--- PMDMMOc/src/com/darkxell/client/state/menu/dungeon/MovesMenuState.java
+++ PMDMMOc/src/com/darkxell/client/state/menu/dungeon/MovesMenuState.java
@@ -10,6 +10,7 @@ import com.darkxell.client.state.dungeon.DungeonState;
 import com.darkxell.client.state.menu.AbstractMenuState;
 import com.darkxell.client.state.menu.components.MoveSelectionWindow;
 import com.darkxell.client.state.menu.components.TextWindow;
+import com.darkxell.client.ui.Keys;
 import com.darkxell.common.pokemon.Pokemon;
 import com.darkxell.common.pokemon.PokemonMove;
 import com.darkxell.common.util.Message;
@@ -57,6 +58,54 @@ public class MovesMenuState extends AbstractMenuState
 	}
 
 	@Override
+	public void onKeyPressed(short key)
+	{
+		if (!Keys.isPressed(Keys.KEY_DIAGONAL))
+		{
+			if (this.tabs.size() != 0)
+			{
+				if (key == Keys.KEY_LEFT && this.tab > 0) --this.tab;
+				else if (key == Keys.KEY_RIGHT && this.tab < this.tabs.size() - 1) ++this.tab;
+				else if (key == Keys.KEY_UP) --this.selection;
+				else if (key == Keys.KEY_DOWN) ++this.selection;
+				else if (key == Keys.KEY_ATTACK) this.onOptionSelected(this.currentOption());
+
+				if (key == Keys.KEY_LEFT || key == Keys.KEY_RIGHT)
+				{
+					if (this.selection >= this.currentTab().options().length) this.selection = this.currentTab().options().length - 1;
+				} else if (key == Keys.KEY_UP || key == Keys.KEY_DOWN)
+				{
+					if (this.selection == -1) this.selection = this.currentTab().options().length - 1;
+					else if (this.selection == this.currentTab().options().length) this.selection = 0;
+				}
+			}
+			if (key == Keys.KEY_MENU || key == Keys.KEY_RUN) this.onExit();
+		} else
+		{
+			boolean success = false;
+			if (key == Keys.KEY_UP && this.selection > 0)
+			{
+				this.pokemon.switchMoves(this.selection, this.selection - 1);
+				--this.selection;
+				success = true;
+			} else if (key == Keys.KEY_DOWN && this.selection < this.currentTab().options().length - 1)
+			{
+				this.pokemon.switchMoves(this.selection, this.selection + 1);
+				++this.selection;
+				success = true;
+			}
+
+			if (success)
+			{
+				MovesMenuState s = new MovesMenuState((DungeonState) this.backgroundState);
+				s.selection = this.selection;
+				s.tab = this.tab;
+				Launcher.stateManager.setState(s);
+			}
+		}
+	}
+
+	@Override
 	protected void onOptionSelected(MenuOption option)
 	{
 		System.out.println(option.name);
@@ -73,7 +122,7 @@ public class MovesMenuState extends AbstractMenuState
 		if (this.windowInfo == null)
 		{
 			Rectangle r = new Rectangle(this.window.dimensions.x, (int) (this.window.dimensions.getMaxY() + 20), width - 40,
-					MenuHudSpriteset.instance.cornerSize.height * 2 + TextRenderer.CHAR_HEIGHT*2);
+					MenuHudSpriteset.instance.cornerSize.height * 2 + TextRenderer.CHAR_HEIGHT * 2);
 			this.windowInfo = new TextWindow(r, new Message("moves.info"), false);
 		}
 		this.windowInfo.render(g, null, width, height);
