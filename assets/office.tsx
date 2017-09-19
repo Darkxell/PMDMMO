0c83d75bf726337050de8927b07d8100eb5d8014
CANDELA Nicolas <darkxell.mc@gmail.com>
2017-09-19 18:04:57 +0200

    animated office flowers

--- assets/office.tsx
+++ assets/office.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="pelipper office" tilewidth="8" tileheight="8" tilecount="3498" columns="66">
- <image source="pelipper office.png" width="528" height="424"/>
+ <image source="office.png" width="528" height="424"/>
 </tileset>
