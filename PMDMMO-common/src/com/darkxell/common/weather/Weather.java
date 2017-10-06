36904e006b540dc0dda42441401fdaa11d4aea4b
Cubiccl <ccl.clement@hotmail.fr>
2017-10-06 23:42:55 +0200

    Northern Range & random weather

--- PMDMMO-common/src/com/darkxell/common/weather/Weather.java
+++ PMDMMO-common/src/com/darkxell/common/weather/Weather.java
@@ -4,5 +4,6 @@ public class Weather
 {
 
 	public static final byte CLEAR = 0, SNOW = 1, HAIL = 2, SUNNY = 3, SANDSTORM = 4, FOG = 5, CLOUDS = 6, RAIN = 7;
+	public static final byte RANDOM = -1;
 
 }
