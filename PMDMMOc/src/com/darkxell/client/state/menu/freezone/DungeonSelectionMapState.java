dd316ba793d18e38bca45343861f4099727936c2
CANDELA Nicolas <darkxell.mc@gmail.com>
2017-10-22 15:20:48 +0200

    Dungeon map state completed

--- PMDMMOc/src/com/darkxell/client/state/menu/freezone/DungeonSelectionMapState.java
+++ PMDMMOc/src/com/darkxell/client/state/menu/freezone/DungeonSelectionMapState.java
@@ -1,13 +1,17 @@
 package com.darkxell.client.state.menu.freezone;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.function.Predicate;
 
 import com.darkxell.client.launchable.Persistance;
 import com.darkxell.client.mechanics.event.ClientEventProcessor;
+import com.darkxell.client.renderers.TextRenderer;
+import com.darkxell.client.resources.images.Hud;
 import com.darkxell.client.resources.images.MapResources;
+import com.darkxell.client.resources.images.MenuHudSpriteset;
 import com.darkxell.client.state.AbstractState;
 import com.darkxell.client.state.FreezoneExploreState;
 import com.darkxell.client.state.dungeon.DungeonState;
@@ -15,6 +19,7 @@ import com.darkxell.client.state.map.DungeonFloorMap;
 import com.darkxell.client.ui.Keys;
 import com.darkxell.common.dungeon.Dungeon;
 import com.darkxell.common.dungeon.DungeonRegistry;
+import com.darkxell.common.util.language.Message;
 
 public class DungeonSelectionMapState extends AbstractState {
 
@@ -52,6 +57,19 @@ public class DungeonSelectionMapState extends AbstractState {
 				cursor = dungeonslist.size() - 1;
 			else
 				--cursor;
+			break;
+		case Keys.KEY_UP:
+			if (cursor < dungeonslist.size() - 10)
+				cursor += 10;
+			else
+				cursor = 0;
+			break;
+		case Keys.KEY_DOWN:
+			if (cursor >= 10)
+				cursor -= 10;
+			else
+				cursor = (dungeonslist.size() - 1);
+			break;
 		case Keys.KEY_RUN:
 			Persistance.stateManager.setState(new FreezoneExploreState());
 			break;
@@ -87,6 +105,22 @@ public class DungeonSelectionMapState extends AbstractState {
 					dungeonslist.get(i).mapy, null);
 		// TRANSLATES THE GRAPHICS BACK
 		g.translate(-translateX, -translateY);
+		// DRAWS THE HUD OVER THE MAP
+		int textxpos = width - Math.max(TextRenderer.instance.width(new Message("dungeonmap.select")),
+				TextRenderer.instance.width(new Message("dungeonmap.return"))) - 10;
+		TextRenderer.instance.render(g, new Message("dungeonmap.select"), textxpos, 20);
+		TextRenderer.instance.render(g, new Message("dungeonmap.return"), textxpos, 40);
+
+		int temp_width = width - 40;
+		int temp_height = temp_width * Hud.textwindow.getHeight() / Hud.textwindow.getWidth();
+		Rectangle box = new Rectangle(20, height - temp_height - 20, temp_width, temp_height);
+		g.drawImage(Hud.textwindow, box.x, box.y, box.width, box.height, null);
+		g.drawImage(MenuHudSpriteset.instance.tabArrowLeft(), box.x + box.width - 45, box.y, null);
+		g.drawImage(MenuHudSpriteset.instance.tabArrowRight(), box.x + box.width - 30, box.y, null);
+		String dungeonsmarker = (this.cursor + 1) + " / " + this.dungeonslist.size();
+		TextRenderer.instance.render(g, dungeonsmarker, box.x + box.width - 55, box.y + 10);
+
+		TextRenderer.instance.render(g, dungeonslist.get(cursor).name(), box.x + 15, box.y + 10);
 	}
 
 	@Override
