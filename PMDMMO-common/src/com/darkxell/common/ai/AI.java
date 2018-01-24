2bb20f93bf2159b16aea5a8066b9297236eceac0
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-01-24 13:58:21 +0100

    AI states

--- PMDMMO-common/src/com/darkxell/common/ai/AI.java
+++ PMDMMO-common/src/com/darkxell/common/ai/AI.java
@@ -2,15 +2,29 @@ package com.darkxell.common.ai;
 
 import com.darkxell.common.dungeon.floor.Floor;
 import com.darkxell.common.event.DungeonEvent;
+import com.darkxell.common.event.TurnSkippedEvent;
 import com.darkxell.common.pokemon.DungeonPokemon;
 
 public abstract class AI
 {
 
+	public static abstract class AIState
+	{
+		public final AI ai;
+
+		public AIState(AI ai)
+		{
+			this.ai = ai;
+		}
+
+		public abstract DungeonEvent takeAction();
+	}
+
 	/** The Floor context. */
 	public final Floor floor;
 	/** The Pokémon this AI controls. */
 	public final DungeonPokemon pokemon;
+	protected AIState state;
 
 	public AI(Floor floor, DungeonPokemon pokemon)
 	{
@@ -18,6 +32,10 @@ public abstract class AI
 		this.pokemon = pokemon;
 	}
 
-	public abstract DungeonEvent takeAction();
+	public DungeonEvent takeAction()
+	{
+		if (this.state == null) return new TurnSkippedEvent(this.floor, this.pokemon);
+		return this.state.takeAction();
+	}
 
 }
