344774a232089c477be0c99de2c57e46a74d42d1
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-01-28 16:54:24 +0100

    If travelling Pokémon are all far, travel is instant

--- PMDMMOc/src/com/darkxell/client/state/dungeon/PokemonTravelState.java
+++ PMDMMOc/src/com/darkxell/client/state/dungeon/PokemonTravelState.java
@@ -7,6 +7,7 @@ import com.darkxell.client.mechanics.animation.TravelAnimation;
 import com.darkxell.client.renderers.floor.PokemonRenderer;
 import com.darkxell.client.resources.images.pokemon.PokemonSprite;
 import com.darkxell.client.state.dungeon.DungeonState.DungeonSubState;
+import com.darkxell.common.dungeon.floor.Tile;
 import com.darkxell.common.dungeon.floor.TileType;
 import com.darkxell.common.event.pokemon.PokemonTravelEvent.PokemonTravel;
 
@@ -18,6 +19,7 @@ public class PokemonTravelState extends DungeonSubState
 
 	private TravelAnimation[] animations;
 	public final int duration;
+	public final boolean faraway;
 	public final boolean running;
 	private int tick;
 	private PokemonTravel[] travels;
@@ -32,6 +34,17 @@ public class PokemonTravelState extends DungeonSubState
 		for (int i = 0; i < this.travels.length; i++)
 			this.animations[i] = new TravelAnimation(this.travels[i].origin.location(), this.travels[i].destination.location());
 		this.tick = 0;
+
+		boolean f = true;
+		Tile camera = this.parent.getCameraPokemon().tile();
+		for (PokemonTravel t : travels)
+			if ((Math.abs(t.origin.x - camera.x) < 10 && Math.abs(t.origin.y - camera.y) < 10)
+					|| (Math.abs(t.destination.x - camera.x) < 10 && Math.abs(t.destination.y - camera.y) < 10))
+			{
+				f = false;
+				break;
+			}
+		this.faraway = f;
 	}
 
 	@Override
@@ -74,6 +87,8 @@ public class PokemonTravelState extends DungeonSubState
 	public void update()
 	{
 		++this.tick;
+		if (this.faraway) this.tick = this.duration;
+
 		float completion = this.tick * 1f / this.duration;
 		for (int i = 0; i < this.travels.length; ++i)
 		{
