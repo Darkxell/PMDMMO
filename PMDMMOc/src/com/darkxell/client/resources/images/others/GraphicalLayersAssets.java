d03fb78c07f80ec215474899cb805d2c1a2c73b0
CANDELA Nicolas <darkxell.mc@gmail.com>
2018-04-05 23:23:51 +0200

    first lsd iteration

--- PMDMMOc/src/com/darkxell/client/resources/images/others/GraphicalLayersAssets.java
+++ PMDMMOc/src/com/darkxell/client/resources/images/others/GraphicalLayersAssets.java
@@ -12,4 +12,8 @@ public abstract class GraphicalLayersAssets {
 	public static BufferedImage[] WATER_horizon = Res.getSpriteRectRow(source_WATER, 0, 208, 48, 128, 5);
 	public static BufferedImage[] WATER_waves = Res.getSpriteRectRow(source_WATER, 240, 208, 48, 168, 10);
 
+	private static final BufferedImage source_LSD = Res.getBase("/graphicallayers/lsd.png");
+	public static BufferedImage[] LSD_top = Res.getSpriteRectRow(source_LSD, 0, 0, 42, 160, 8);
+	public static BufferedImage[] LSD_bot = Res.getSpriteRectRow(source_LSD, 0, 160, 42, 160, 8);
+
 }
