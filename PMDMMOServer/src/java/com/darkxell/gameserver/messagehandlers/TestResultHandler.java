56c57f595739a4dbc6faccfbf680fa64db9bf97a
CANDELA Nicolas <darkxell.mc@gmail.com>
2018-05-17 01:03:27 +0200

    testresulthandler now sets storyposition

--- PMDMMOServer/src/java/com/darkxell/gameserver/messagehandlers/TestResultHandler.java
+++ PMDMMOServer/src/java/com/darkxell/gameserver/messagehandlers/TestResultHandler.java
@@ -5,6 +5,7 @@
  */
 package com.darkxell.gameserver.messagehandlers;
 
+import com.darkxell.common.dbobject.DBPlayer;
 import com.darkxell.common.pokemon.Pokemon;
 import com.darkxell.common.pokemon.PokemonRegistry;
 import com.darkxell.common.pokemon.PokemonSpecies;
@@ -13,6 +14,7 @@ import com.darkxell.gameserver.GameSessionHandler;
 import com.darkxell.gameserver.GameSessionInfo;
 import com.darkxell.gameserver.MessageHandler;
 import com.darkxell.gameserver.SessionsInfoHolder;
+import com.eclipsesource.json.Json;
 import java.util.Random;
 import javax.json.JsonObject;
 import javax.websocket.Session;
@@ -22,11 +24,11 @@ import javax.websocket.Session;
  * @author Darkxell
  */
 public class TestResultHandler extends MessageHandler {
-
+    
     public TestResultHandler(GameServer endpoint) {
         super(endpoint);
     }
-
+    
     @Override
     public void handleMessage(JsonObject json, Session from, GameSessionHandler sessionshandler) {
         GameSessionInfo si = SessionsInfoHolder.getInfo(from.getId());
@@ -37,18 +39,31 @@ public class TestResultHandler extends MessageHandler {
         int offid = json.getJsonNumber("offid").intValue();
         int maingender = json.getJsonNumber("maingender").intValue();
         int offgender = json.getJsonNumber("offgender").intValue();
-        // TODO : Check that mainID is a valid number
+        // TODO : Check that mainID is a valid number and storyposition = 0
         
         PokemonSpecies espece = PokemonRegistry.find(mainid);
         Pokemon main = espece.generate(new Random(), 5);
         espece = PokemonRegistry.find(offid);
         Pokemon off = espece.generate(new Random(), 5);
         
-        // PokemonDBUtilities.savePokemonInDatabase(main, endpoint, si.serverid, (byte)1);
-        // PokemonDBUtilities.savePokemonInDatabase(off, endpoint, si.serverid, (byte)2);
+        long t = endpoint.getPokemonDAO().create(main.getData());
+        if (t > 0) {
+            endpoint.getTeammember_DAO().create(si.serverid, t, (byte) 1);
+        }
+        t = endpoint.getPokemonDAO().create(off.getData());
+        if (t > 0) {
+            endpoint.getTeammember_DAO().create(si.serverid, t, (byte) 2);
+        }
+        DBPlayer pl = endpoint.getPlayerDAO().find(si.serverid);
+        pl.storyposition = 1;
+        endpoint.getPlayerDAO().update(pl);
         
-        System.out.println("recieved test result : " + mainid +" and "+ offid);
+        System.out.println("recieved test result : " + mainid + " and " + offid);
+        
+        com.eclipsesource.json.JsonObject value = Json.object();
+        value.add("action", "testresultrecieved");
+        sessionshandler.sendToSession(from, value);
         
     }
-
+    
 }
