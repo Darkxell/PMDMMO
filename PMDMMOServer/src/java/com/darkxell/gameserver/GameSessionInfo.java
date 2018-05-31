311815bbf3f3a1c5bdb895ba34ebacaab3f99b33
CANDELA Nicolas <darkxell.mc@gmail.com>
2018-06-01 00:56:25 +0200

    Itemactionhandler done

--- PMDMMOServer/src/java/com/darkxell/gameserver/GameSessionInfo.java
+++ PMDMMOServer/src/java/com/darkxell/gameserver/GameSessionInfo.java
@@ -7,22 +7,21 @@ package com.darkxell.gameserver;
 
 /**
  * A GameSessionInfo object describes the attributes of a session.
+ *
  * @author Darkxell
  */
 public class GameSessionInfo {
-    
+
     // COMMON DATA
-    
     public boolean isconnected = false;
     public String salt = "";
     public String name = "DefaultServerName";
-    public long serverid = 0l;
-    
+    public long serverid = -1l;//0 is the "not found" value. Default should not be 0.
+
     // FREEZONE DATA
-    
     public String currentPokemon = "";
     public String freezoneID = "undefined";
     public double posFX = 0;
     public double posFY = 0;
-    
+
 }
