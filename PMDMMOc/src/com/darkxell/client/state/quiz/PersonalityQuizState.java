af8aadb5ea0baebaafb1225db7248bb584ff8251
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-06-17 10:10:38 +0200

    Hotfix

--- PMDMMOc/src/com/darkxell/client/state/quiz/PersonalityQuizState.java
+++ PMDMMOc/src/com/darkxell/client/state/quiz/PersonalityQuizState.java
@@ -13,6 +13,7 @@ import com.darkxell.client.resources.images.pokemon.PokemonPortrait;
 import com.darkxell.client.resources.music.SoundsHolder;
 import com.darkxell.client.state.AbstractState;
 import com.darkxell.client.state.PlayerLoadingState;
+import com.darkxell.client.state.PlayerLoadingState.PlayerLoadingEndListener;
 import com.darkxell.client.state.dialog.AbstractDialogState;
 import com.darkxell.client.state.dialog.AbstractDialogState.DialogEndListener;
 import com.darkxell.client.state.dialog.DialogScreen;
@@ -28,7 +29,8 @@ import com.darkxell.common.util.language.Message;
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonObject;
 
-public class PersonalityQuizState extends AbstractState implements DialogEndListener {
+public class PersonalityQuizState extends AbstractState implements DialogEndListener
+{
 	private static final int startDialogScreens = 7, endDialogScreens = 3;
 
 	private BackgroundLsdLayer background;
@@ -43,7 +45,8 @@ public class PersonalityQuizState extends AbstractState implements DialogEndList
 	private Pokemon starter;
 	private int tick = 0;
 
-	public PersonalityQuizState() {
+	public PersonalityQuizState()
+	{
 		this.background = new BackgroundLsdLayer();
 		this.quizData = new QuizData();
 		this.quizData.chooseQuestions(8);
@@ -52,80 +55,76 @@ public class PersonalityQuizState extends AbstractState implements DialogEndList
 		this.currentQuestion = -1;
 	}
 
-	private void loadNewDialog() {
+	private void loadNewDialog()
+	{
 		this.currentDialog = null;
-		if (this.currentQuestion == this.questions.length + 4) {
+		if (this.currentQuestion == this.questions.length + 4)
+		{
 			ArrayList<DialogScreen> screens = new ArrayList<>();
 			for (int i = 0; i < endDialogScreens; ++i)
 				screens.add(new DialogScreen(new Message("quiz.end." + i)));
 			this.currentDialog = new DialogState(this, this, true, screens);
-		} else if (this.currentQuestion == this.questions.length + 3)
-			this.currentDialog = new PartnerChoiceState(this);
-		else if (this.currentQuestion == this.questions.length + 2)
-			this.currentDialog = new DialogState(this, this, true, new DialogScreen(
-					new Message("quiz.starter").addReplacement("<pokemon>", this.starter.getNickname())));
-		else if (this.currentQuestion == this.questions.length + 1)
-			this.currentDialog = new DialogState(this, this, this.finalNature.description());
-		else if (this.currentQuestion == this.questions.length)
-			this.currentDialog = new OptionDialogState(this, this,
-					Arrays.asList(new DialogScreen(new Message("quiz.gender"))), new Message("quiz.gender.boy"),
-					new Message("quiz.gender.girl"));
-		else if (this.currentQuestion >= 0 && this.currentQuestion < this.questions.length)
-			this.currentDialog = new OptionDialogState(this, this,
-					Arrays.asList(new DialogScreen(this.questions[this.currentQuestion].name)),
-					this.questions[this.currentQuestion].options());
+		} else if (this.currentQuestion == this.questions.length + 3) this.currentDialog = new PartnerChoiceState(this);
+		else if (this.currentQuestion == this.questions.length + 2) this.currentDialog = new DialogState(this, this, true,
+				new DialogScreen(new Message("quiz.starter").addReplacement("<pokemon>", this.starter.getNickname())));
+		else if (this.currentQuestion == this.questions.length + 1) this.currentDialog = new DialogState(this, this, this.finalNature.description());
+		else if (this.currentQuestion == this.questions.length) this.currentDialog = new OptionDialogState(this, this,
+				Arrays.asList(new DialogScreen(new Message("quiz.gender"))), new Message("quiz.gender.boy"), new Message("quiz.gender.girl"));
+		else if (this.currentQuestion >= 0 && this.currentQuestion < this.questions.length) this.currentDialog = new OptionDialogState(this, this,
+				Arrays.asList(new DialogScreen(this.questions[this.currentQuestion].name)), this.questions[this.currentQuestion].options());
 
-		if (this.currentDialog == null)
-			Persistance.stateManager.setState(this);
-		else
-			Persistance.stateManager.setState(this.currentDialog);
+		if (this.currentDialog == null) Persistance.stateManager.setState(this);
+		else Persistance.stateManager.setState(this.currentDialog);
 	}
 
 	@Override
-	public void onDialogEnd(AbstractDialogState dialog) {
+	public void onDialogEnd(AbstractDialogState dialog)
+	{
 
-		if (this.currentQuestion >= 0 && this.currentQuestion < this.questions.length) {
-			QuizAnswer answer = this.questions[this.currentQuestion].answers[((OptionDialogState) dialog)
-					.chosenIndex()];
+		if (this.currentQuestion >= 0 && this.currentQuestion < this.questions.length)
+		{
+			QuizAnswer answer = this.questions[this.currentQuestion].answers[((OptionDialogState) dialog).chosenIndex()];
 			for (int i = 0; i < answer.natures.length; ++i)
 				this.currentNature[answer.natures[i].id] += answer.points[i];
 		}
 
 		++this.currentQuestion;
 
-		if (this.currentQuestion == this.questions.length) {
+		if (this.currentQuestion == this.questions.length)
+		{
 			this.finalNature = Nature.Brave;
 			for (int i = 0; i < this.currentNature.length; ++i)
-				if (this.currentNature[this.finalNature.id] < this.currentNature[i])
-					this.finalNature = Nature.get(i);
-		} else if (this.currentQuestion == this.questions.length + 1) {
+				if (this.currentNature[this.finalNature.id] < this.currentNature[i]) this.finalNature = Nature.get(i);
+		} else if (this.currentQuestion == this.questions.length + 1)
+		{
 			this.gender = ((OptionDialogState) dialog).chosenIndex() == 0 ? QuizGender.Boy : QuizGender.Girl;
 			int starterID = this.quizData.starters[this.finalNature.id][this.gender == QuizGender.Boy ? 0 : 1];
 			this.starter = PokemonRegistry.find(starterID).generate(new Random(), 5);
-		} else if (this.currentQuestion == this.questions.length + 4) {
+		} else if (this.currentQuestion == this.questions.length + 4)
+		{
 			int index = ((OptionDialogState) dialog).chosenIndex();
 			this.partner = this.partners()[index].generate(new Random(), 5);
 		}
 
-		if (this.currentQuestion == 0)
-			Persistance.stateManager.setState(this);
-		else
-			this.loadNewDialog();
+		if (this.currentQuestion == 0) Persistance.stateManager.setState(this);
+		else this.loadNewDialog();
 	}
 
 	@Override
-	public void onKeyPressed(short key) {
-	}
+	public void onKeyPressed(short key)
+	{}
 
 	@Override
-	public void onKeyReleased(short key) {
-	}
+	public void onKeyReleased(short key)
+	{}
 
 	@Override
-	public void onStart() {
+	public void onStart()
+	{
 		super.onStart();
 
-		if (this.currentQuestion == -1) {
+		if (this.currentQuestion == -1)
+		{
 			Persistance.soundmanager.setBackgroundMusic(SoundsHolder.getSong("personality-test.mp3"));
 			ArrayList<DialogScreen> screens = new ArrayList<>();
 			for (int i = 0; i < startDialogScreens; ++i)
@@ -134,11 +133,13 @@ public class PersonalityQuizState extends AbstractState implements DialogEndList
 		}
 	}
 
-	public PokemonSpecies[] partners() {
+	public PokemonSpecies[] partners()
+	{
 		return this.quizData.validPartners(this.starter.species().id);
 	}
 
-	public Message[] partnersAsOptions() {
+	public Message[] partnersAsOptions()
+	{
 		PokemonSpecies[] partners = this.partners();
 		Message[] options = new Message[partners.length];
 		for (int i = 0; i < options.length; ++i)
@@ -147,21 +148,21 @@ public class PersonalityQuizState extends AbstractState implements DialogEndList
 	}
 
 	@Override
-	public void render(Graphics2D g, int width, int height) {
+	public void render(Graphics2D g, int width, int height)
+	{
 		this.background.render(g, width, height);
 		if (this.currentQuestion == this.questions.length + 2)
-			PokemonPortrait.drawPortrait(g, this.starter, width / 2 - PokemonPortrait.PORTRAIT_SIZE / 2,
-					height / 2 - PokemonPortrait.PORTRAIT_SIZE / 2);
+			PokemonPortrait.drawPortrait(g, this.starter, width / 2 - PokemonPortrait.PORTRAIT_SIZE / 2, height / 2 - PokemonPortrait.PORTRAIT_SIZE / 2);
 
-		if (this.tick <= NarratorDialogState.FADETIME) {
+		if (this.tick <= NarratorDialogState.FADETIME)
+		{
 			double alpha = 255 - (((double) this.tick) / NarratorDialogState.FADETIME) * 255;
-			if (alpha < 0)
-				alpha = 0;
-			else if (alpha > 255)
-				alpha = 255;
+			if (alpha < 0) alpha = 0;
+			else if (alpha > 255) alpha = 255;
 			g.setColor(new Color(0, 0, 0, (int) alpha));
 			g.fillRect(0, 0, width, height);
-			if (this.tick < 0) {
+			if (this.tick < 0)
+			{
 				g.setColor(Color.WHITE);
 				TextRenderer.render(g, "Loading. . .", 20, 20);
 			}
@@ -169,33 +170,34 @@ public class PersonalityQuizState extends AbstractState implements DialogEndList
 	}
 
 	@Override
-	public void update() {
-		if (this.isMain() && this.currentQuestion == 0) {
-			if (this.tick <= NarratorDialogState.FADETIME)
-				++this.tick;
-			if (this.tick == NarratorDialogState.FADETIME)
-				this.loadNewDialog();
+	public void update()
+	{
+		if (this.isMain() && this.currentQuestion == 0)
+		{
+			if (this.tick <= NarratorDialogState.FADETIME) ++this.tick;
+			if (this.tick == NarratorDialogState.FADETIME) this.loadNewDialog();
 		}
-		if (this.isMain() && this.currentQuestion > this.questions.length + 3) {
-			if (this.tick <= NarratorDialogState.FADETIME && this.tick >= -10)
-				--this.tick;
-			if (this.tick == 0) {
+		if (this.isMain() && this.currentQuestion > this.questions.length + 3)
+		{
+			if (this.tick <= NarratorDialogState.FADETIME && this.tick >= -10) --this.tick;
+			if (this.tick == 0)
+			{
 				sendTestResults(this.starter.species().id, this.partner.species().id, this.starter.gender(), this.starter.gender());
 			}
 		}
 		this.background.update();
 	}
 
-	private void sendTestResults(int id1, int id2, int gender1, int gender2) {
-		JsonObject tosend = Json.object().add("action", "testresult").add("mainid", id1).add("offid", id2)
-				.add("maingender", gender1).add("offgender", gender2);
+	private void sendTestResults(int id1, int id2, int gender1, int gender2)
+	{
+		JsonObject tosend = Json.object().add("action", "testresult").add("mainid", id1).add("offid", id2).add("maingender", gender1).add("offgender", gender2);
 		Persistance.socketendpoint.sendMessage(tosend.toString());
 		Logger.i("Chosen pokemons " + id1 + " and " + id2 + ", sending to server.");
 	}
 
 	public void onResultConfirmed()
 	{
-		Persistance.stateManager.setState(new PlayerLoadingState(Persistance.player.getData().id));
+		Persistance.stateManager.setState(new PlayerLoadingState(Persistance.player.getData().id, new PlayerLoadingEndListener() {}));
 	}
 
 }
