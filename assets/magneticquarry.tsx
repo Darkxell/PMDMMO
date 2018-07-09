b4c1448acec6f4dc4fba1186e4895bb9142849cc
CANDELA Nicolas <darkxell.mc@gmail.com>
2018-07-09 17:44:10 +0200

    working on tmx files part 3

---
+++ assets/magneticquarry.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="magneticquarry" tilewidth="8" tileheight="8" tilecount="1800" columns="60">
+ <image source="friends/magneticquarry.png" width="480" height="240"/>
+</tileset>
---
+++ assets/mtdiscipline.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="mtdiscipline" tilewidth="8" tileheight="8" tilecount="2419" columns="59">
+ <image source="friends/mtdiscipline.png" width="472" height="328"/>
+</tileset>
