55ba40541aad59a7b5e8f0487b8c08d4e9bd2538
CANDELA Nicolas <darkxell.mc@gmail.com>
2018-07-10 18:33:03 +0200

    working on tmx files part 4

---
+++ assets/stratoslookout.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="stratoslookout" tilewidth="8" tileheight="8" tilecount="1560" columns="40">
+ <image source="friends/stratoslookout.png" width="320" height="312"/>
+</tileset>
---
+++ assets/tadpolepond.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tadpolepond" tilewidth="8" tileheight="8" tilecount="2646" columns="63">
+ <image source="friends/tadpolepond.png" width="504" height="336"/>
+</tileset>
