067990e2e9ec58a6a3cdb1afaf779eb5b4878c90
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-07-12 15:36:20 +0200

    Rotate Event

--- PMDMMOc/src/com/darkxell/client/mechanics/cutscene/event/RotateCutsceneEvent.java
+++ PMDMMOc/src/com/darkxell/client/mechanics/cutscene/event/RotateCutsceneEvent.java
@@ -25,6 +25,14 @@ public class RotateCutsceneEvent extends CutsceneEvent
 		this.distance = XMLUtils.getAttribute(xml, "distance", 0);
 	}
 
+	public RotateCutsceneEvent(int id, CutsceneEntity entity, int distance, boolean instantly)
+	{
+		super(id, CutsceneEventType.rotate);
+		this.target = entity == null ? -1 : entity.id;
+		this.distance = distance;
+		this.instantly = instantly;
+	}
+
 	@Override
 	public boolean isOver()
 	{
@@ -52,6 +60,22 @@ public class RotateCutsceneEvent extends CutsceneEvent
 	}
 
 	@Override
+	public String toString()
+	{
+		return this.displayID() + "(" + this.target + ") rotates clockwise " + this.distance + " times";
+	}
+
+	@Override
+	public Element toXML()
+	{
+		Element root = super.toXML();
+		root.setAttribute("target", String.valueOf(this.target));
+		root.setAttribute("distance", String.valueOf(this.distance));
+		XMLUtils.setAttribute(root, "instantly", this.instantly, false);
+		return root;
+	}
+
+	@Override
 	public void update()
 	{
 		super.update();
@@ -61,12 +85,6 @@ public class RotateCutsceneEvent extends CutsceneEvent
 			else this.pokemon.facing = this.pokemon.facing.rotateCounterClockwise();
 		}
 		++this.currentDistance;
-	}
-
-	@Override
-	public String toString()
-	{
-		return this.displayID() + "(" + this.target + ") rotates clockwise " + this.distance + " times";
 	}
 
 }
