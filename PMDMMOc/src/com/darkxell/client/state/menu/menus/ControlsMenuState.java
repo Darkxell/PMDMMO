c27b91959c2f73efefd49513d43553f5f1c562e8
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-07-16 13:48:42 +0200

    Controls menu displays

--- PMDMMOc/src/com/darkxell/client/state/menu/menus/ControlsMenuState.java
+++ PMDMMOc/src/com/darkxell/client/state/menu/menus/ControlsMenuState.java
@@ -6,9 +6,10 @@ import com.darkxell.client.launchable.Persistance;
 import com.darkxell.client.state.AbstractState;
 import com.darkxell.client.state.mainstates.PrincipalMainState;
 import com.darkxell.client.state.menu.AbstractMenuState;
+import com.darkxell.client.state.menu.OptionSelectionMenuState;
 import com.darkxell.client.ui.Keys.Key;
 
-public class ControlsMenuState extends AbstractMenuState
+public class ControlsMenuState extends OptionSelectionMenuState
 {
 
 	public static class ControlMenuOption extends MenuOption
@@ -25,6 +26,11 @@ public class ControlsMenuState extends AbstractMenuState
 		}
 	}
 
+	private static final String[] tabNames = { "movement", "actions", "menus", "map" };
+	private static final Key[][] tabStructure = { { Key.UP, Key.DOWN, Key.LEFT, Key.RIGHT, Key.RUN, Key.DIAGONAL, Key.ROTATE },
+			{ Key.ATTACK, Key.MOVE_1, Key.MOVE_2, Key.MOVE_3, Key.MOVE_4, Key.ITEM_1, Key.ITEM_2 },
+			{ Key.MENU, Key.INVENTORY, Key.PARTY, Key.PAGE_LEFT, Key.PAGE_RIGHT }, { Key.MAP_UP, Key.MAP_DOWN, Key.MAP_LEFT, Key.MAP_RIGHT, Key.MAP_RESET } };
+
 	public final AbstractMenuState parent;
 
 	public ControlsMenuState(AbstractMenuState parent, AbstractState backgroundState)
@@ -38,16 +44,24 @@ public class ControlsMenuState extends AbstractMenuState
 	@Override
 	protected void createOptions()
 	{
-		MenuTab tab = new MenuTab("menu.controls");
-		for (Key key : Key.values())
-			tab.addOption(new ControlMenuOption(key));
-		this.tabs.add(tab);
+
+		for (int tab = 0; tab < tabStructure.length; ++tab)
+		{
+			MenuTab t = new MenuTab("key.tab." + tabNames[tab]);
+			t.name.addSuffix(" (" + (tab + 1) + "/" + tabStructure.length + ")");
+
+			for (int menu = 0; menu < tabStructure[tab].length; ++menu)
+				t.addOption(new ControlMenuOption(tabStructure[tab][menu]));
+
+			this.tabs.add(t);
+		}
 	}
 
 	@Override
 	protected Rectangle mainWindowDimensions()
 	{
-		return new Rectangle(16, 32, PrincipalMainState.displayWidth - 16 * 2, PrincipalMainState.displayHeight - 32 * 2);
+		Rectangle r = super.mainWindowDimensions();
+		return new Rectangle(r.x, r.y, PrincipalMainState.displayWidth / 2, r.height);
 	}
 
 	@Override
