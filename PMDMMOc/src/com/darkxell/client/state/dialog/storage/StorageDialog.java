2b1dcfe8f605de551d5ff1978be0aa08024561d4
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-07-17 16:06:57 +0200

    Storage dialog base

---
+++ PMDMMOc/src/com/darkxell/client/state/dialog/storage/StorageDialog.java
@@ -0,0 +1,65 @@
+package com.darkxell.client.state.dialog.storage;
+
+import com.darkxell.client.renderers.layers.AbstractGraphiclayer;
+import com.darkxell.client.state.AbstractState;
+import com.darkxell.client.state.dialog.ComplexDialog;
+import com.darkxell.client.state.dialog.DialogScreen;
+import com.darkxell.client.state.dialog.DialogState;
+import com.darkxell.client.state.dialog.OptionDialogScreen;
+import com.darkxell.common.pokemon.PokemonRegistry;
+import com.darkxell.common.pokemon.PokemonSpecies;
+import com.darkxell.common.util.language.Message;
+
+public class StorageDialog extends ComplexDialog
+{
+	private static final byte ACTION = 1;
+	@SuppressWarnings("unused")
+	private static final int DEPOSIT = 0, WITHDRAW = 1, EXIT = 2;
+
+	private int selectedAction;
+	private PokemonSpecies shopkeeper;
+
+	public StorageDialog(AbstractGraphiclayer background)
+	{
+		super(background);
+		this.shopkeeper = PokemonRegistry.find(115);
+	}
+
+	private DialogState actionSelection(boolean isFirst)
+	{
+		return this.newDialog(new OptionDialogScreen(this.shopkeeper, new Message("dialog.storage.intro"), new Message("dialog.storage.deposit"),
+				new Message("dialog.storage.withdraw"), new Message("dialog.storage.exit")).setID(ACTION)).setOpaque(true);
+	}
+
+	@Override
+	public DialogState firstState()
+	{
+		return this.actionSelection(true);
+	}
+
+	@Override
+	public ComplexDialogAction nextAction(DialogState previous)
+	{
+		return ComplexDialogAction.TERMINATE;
+	}
+
+	@Override
+	public DialogState nextState(DialogState previous)
+	{
+		return this.actionSelection(false);
+	}
+
+	@Override
+	protected void onDialogFinished(DialogState dialog)
+	{
+		DialogScreen screen = dialog.currentScreen();
+		if (screen.id != ACTION) this.selectedAction = ((OptionDialogScreen) screen).chosenIndex();
+	}
+
+	@Override
+	public AbstractState onFinish(DialogState lastState)
+	{
+		return (AbstractState) this.background;
+	}
+
+}
