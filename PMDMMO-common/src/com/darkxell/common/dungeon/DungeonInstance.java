6a71ad13b840c615a82dfb15b6c1e8bcd85894be
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-07-31 10:13:47 +0200

    3-9 is playable

--- PMDMMO-common/src/com/darkxell/common/dungeon/DungeonInstance.java
+++ PMDMMO-common/src/com/darkxell/common/dungeon/DungeonInstance.java
@@ -177,7 +177,7 @@ public class DungeonInstance
 	{
 		this.isGeneratingFloor = true;
 		if (this.currentFloor != null) this.currentFloor.dispose();
-		this.currentFloor = this.createFloor(this.currentFloor == null ? 1 : this.currentFloor.id + 1);
+		this.currentFloor = this.createFloor(this.currentFloor == null ? this.dungeon().floorCount : this.currentFloor.id + 1);// TODO Change 'flooCount' back to '1'
 		this.currentFloor.generate();
 		this.currentFloor.placePlayers(this.exploringPlayers);
 
