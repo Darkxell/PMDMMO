0c25bcbfd6d90ecc6f467d4943020bb884dd8072
CANDELA Nicolas <darkxell.mc@gmail.com>
2018-08-03 00:00:27 +0200

    Preparing merge

--- PMDMMOc/src/com/darkxell/client/state/menu/freezone/MissionBoardState.java
+++ PMDMMOc/src/com/darkxell/client/state/menu/freezone/MissionBoardState.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 import com.darkxell.client.launchable.Persistance;
+import com.darkxell.client.renderers.TextRenderer;
 import com.darkxell.client.state.AbstractState;
 import com.darkxell.client.ui.Keys.Key;
 
@@ -21,6 +22,8 @@ public class MissionBoardState extends AbstractState {
 		case RUN:
 			Persistance.stateManager.setState(new MissionBoardSelectionState(this.exploresource));
 			break;
+		default:
+			break;
 		}
 	}
 
@@ -31,13 +34,19 @@ public class MissionBoardState extends AbstractState {
 
 	@Override
 	public void render(Graphics2D g, int width, int height) {
+		this.exploresource.render(g, width, height);
 		g.setColor(new Color(100, 100, 100, 100));
 		g.fillRect(0, 0, width, height);
+
+		String s = "WIP, this was my branch. 4th wall demolition hello.";
+		TextRenderer.render(g, s, width / 2 - (TextRenderer.width(s) / 2), height / 2);
+		s = "Press return to go back to explorestate.";
+		TextRenderer.render(g, s, width / 2 - (TextRenderer.width(s) / 2), height / 2 + 20);
 	}
 
 	@Override
 	public void update() {
-
+		this.exploresource.update();
 	}
 
 }
