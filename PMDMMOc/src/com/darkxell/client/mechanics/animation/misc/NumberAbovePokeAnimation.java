1ab289542378fde3ee6f9418bb2032f9d8983541
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-08-12 14:38:40 +0200

    Damage numbers fade out

--- PMDMMOc/src/com/darkxell/client/mechanics/animation/misc/NumberAbovePokeAnimation.java
+++ PMDMMOc/src/com/darkxell/client/mechanics/animation/misc/NumberAbovePokeAnimation.java
@@ -1,5 +1,7 @@
 package com.darkxell.client.mechanics.animation.misc;
 
+import java.awt.AlphaComposite;
+import java.awt.Composite;
 import java.awt.Graphics2D;
 
 import com.darkxell.client.mechanics.animation.PokemonAnimation;
@@ -9,20 +11,22 @@ import com.darkxell.common.pokemon.DungeonPokemon;
 
 public class NumberAbovePokeAnimation extends PokemonAnimation
 {
+	public static final int DURATION = 60;
+	public static final int FADE = 40;
 
 	public final FontMode fontMode;
 	public final int number;
 
 	public NumberAbovePokeAnimation(DungeonPokemon target, int number, FontMode fontMode)
 	{
-		super(target, 60, null);
+		super(target, DURATION, null);
 		this.number = number;
 		this.fontMode = fontMode;
 	}
 
 	private double getHealthPos()
 	{
-		return (60 - this.tick()) / 4;
+		return (DURATION - this.tick()) / 4;
 	}
 
 	@Override
@@ -30,10 +34,19 @@ public class NumberAbovePokeAnimation extends PokemonAnimation
 	{
 		super.postrender(g, width, height);
 
+		float alpha = 1;
+		if (this.tick() >= FADE) alpha = (DURATION - this.tick()) * 1f / (DURATION - FADE);
+
+		AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha);
+		Composite c = g.getComposite();
+		if (alpha != 1) g.setComposite(ac);
+
 		String text = (this.number < 0 ? "" : "+") + Integer.toString(this.number);
 		int xPos = (int) (this.x - TextRenderer.width(text) / 2);
 		int yPos = (int) (this.y + this.getHealthPos() - this.renderer.sprite().getCurrentSprite().getHeight() / 2 - TextRenderer.height() / 2);
 		TextRenderer.render(g, text, xPos, yPos, this.fontMode);
+
+		if (alpha != 1) g.setComposite(c);
 	}
 
 }
