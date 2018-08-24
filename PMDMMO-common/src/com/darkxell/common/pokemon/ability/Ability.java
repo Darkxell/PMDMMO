d23a937865aecf2a6f95378e08afaf882548ed1f
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-08-24 11:58:42 +0200

    Ability: 36- Poison Point

--- PMDMMO-common/src/com/darkxell/common/pokemon/ability/Ability.java
+++ PMDMMO-common/src/com/darkxell/common/pokemon/ability/Ability.java
@@ -7,6 +7,7 @@ import com.darkxell.common.pokemon.AffectsPokemon;
 import com.darkxell.common.pokemon.BaseStats.Stat;
 import com.darkxell.common.pokemon.DungeonPokemon;
 import com.darkxell.common.pokemon.PokemonType;
+import com.darkxell.common.status.StatusCondition;
 import com.darkxell.common.util.language.Lang;
 import com.darkxell.common.util.language.Message;
 import com.darkxell.common.weather.Weather;
@@ -25,6 +26,8 @@ public abstract class Ability implements AffectsPokemon, DungeonEventListener
 	public static final Ability LEVITATE = new AbilityNullifyType(16, PokemonType.Ground);
 	public static final Ability SOUNDPROOF = new AbilityNullifySound(19);
 	public static final Ability WONDER_GUARD = new AbilityNullifyNonSupEff(21);
+	
+	public static final Ability POISON_POINT = new AbilityStatusOnHit(36, StatusCondition.Poisoned, 12);
 
 	public static final Ability KEEN_EYE = new AbilityPreventsStatLoss(46, Stat.Accuracy);
 
---
+++ PMDMMO-common/src/com/darkxell/common/pokemon/ability/AbilityStatusOnHit.java
@@ -0,0 +1,29 @@
+package com.darkxell.common.pokemon.ability;
+
+import java.util.ArrayList;
+
+import com.darkxell.common.dungeon.floor.Floor;
+import com.darkxell.common.event.DungeonEvent;
+import com.darkxell.common.event.move.MoveSelectionEvent.MoveUse;
+import com.darkxell.common.event.pokemon.DamageDealtEvent;
+import com.darkxell.common.event.pokemon.StatusConditionCreatedEvent;
+import com.darkxell.common.status.StatusCondition;
+
+public class AbilityStatusOnHit extends AbilityOnHit
+{
+
+	public final StatusCondition condition;
+
+	public AbilityStatusOnHit(int id, StatusCondition condition, int probability)
+	{
+		super(id, probability);
+		this.condition = condition;
+	}
+
+	@Override
+	protected void onHit(Floor floor, DamageDealtEvent event, MoveUse source, ArrayList<DungeonEvent> resultingEvents)
+	{
+		resultingEvents.add(new StatusConditionCreatedEvent(floor, this.condition.create(source.user, this, floor.random)));
+	}
+
+}
