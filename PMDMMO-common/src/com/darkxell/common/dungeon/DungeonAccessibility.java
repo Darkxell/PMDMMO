0021d60125214af4d826f2655872237f4d438323
CANDELA Nicolas <darkxell.mc@gmail.com>
2018-08-29 14:23:42 +0200

    added storypositions for early dungeons

--- PMDMMO-common/src/com/darkxell/common/dungeon/DungeonAccessibility.java
+++ PMDMMO-common/src/com/darkxell/common/dungeon/DungeonAccessibility.java
@@ -14,41 +14,41 @@ public class DungeonAccessibility{
     case 1://TINY WOODS
     return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
     case 2://THUNDERWAVE CAVE
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=5)?ACCESSIBLE:LOCKED;
     case 3://MOUNT STEEL
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 4://SINISTER WOODS
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 5://SILENT CHASM
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 6://MOUNT THUNDER
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 7://MOUNT THUNDER PEAK
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 8://GREAT CANYON
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 9://LAPIS CAVE
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 10://MOUNT BLAZE
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 11://MOUNT BLAZE PEAK
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 12://FROSTY FOREST
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 13://FROSTY GROTTO
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 14://MOUNT FREEZE
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 15://MOUNT FREEZE PEAK
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 16://MAGMA CAVERN
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 17://MAGMA CAVERN PIT
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 18://SKY TOWER
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     case 19://SKY TOWER SUMMIT
-    return (player.storyposition>=1)?ACCESSIBLE:LOCKED;
+    return (player.storyposition>=15)?ACCESSIBLE:LOCKED;
     default:
     return false;
     }
