7e060da300c52d453c61816fdf30f46640a5ae66
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-09-06 16:44:22 +0200

    Move: 359- Mud-Slap

--- PMDMMO-common/src/com/darkxell/common/util/Util.java
+++ PMDMMO-common/src/com/darkxell/common/util/Util.java
@@ -16,7 +16,7 @@ public class Util
 
 	public static Player createDefaultPlayer()
 	{
-		Player player = new Player("Offline debug account name", PokemonRegistry.find(255).generate(new Random(), 10, 1));
+		Player player = new Player("Offline debug account name", PokemonRegistry.find(258).generate(new Random(), 10, 1));
 		player.setStoryPosition(500);
 		player.setMoneyInBag(100);
 		player.setMoneyInBank(456789);
