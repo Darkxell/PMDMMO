7ba982f73b9ca6b40117f9426d0e44db344532ec
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-09-25 11:44:34 +0200

    Item: 152- Max Elixir

--- PMDMMO-common/src/com/darkxell/common/item/effects/ElixirItemEffect.java
+++ PMDMMO-common/src/com/darkxell/common/item/effects/ElixirItemEffect.java
@@ -1,5 +1,13 @@
 package com.darkxell.common.item.effects;
 
+import java.util.ArrayList;
+
+import com.darkxell.common.dungeon.floor.Floor;
+import com.darkxell.common.event.DungeonEvent;
+import com.darkxell.common.event.stats.PPRestoredEvent;
+import com.darkxell.common.item.Item;
+import com.darkxell.common.pokemon.DungeonPokemon;
+
 /** An Item that restores PP when eaten. */
 public class ElixirItemEffect extends DrinkItemEffect
 {
@@ -16,6 +24,13 @@ public class ElixirItemEffect extends DrinkItemEffect
 	public boolean isUsedOnTeamMember()
 	{
 		return true;
+	}
+
+	@Override
+	public void use(Floor floor, Item item, DungeonPokemon pokemon, DungeonPokemon target, ArrayList<DungeonEvent> events)
+	{
+		super.use(floor, item, pokemon, target, events);
+		events.add(new PPRestoredEvent(floor, target, this.pp));
 	}
 
 }
--- PMDMMO-common/src/com/darkxell/common/util/Util.java
+++ PMDMMO-common/src/com/darkxell/common/util/Util.java
@@ -37,6 +37,7 @@ public class Util
 			player.inventory().addItem(new ItemStack(i, 6));
 		player.inventory().addItem(new ItemStack(31, 3));
 		player.inventory().addItem(new ItemStack(42, 3));
+		player.inventory().addItem(new ItemStack(152));
 		player.inventory().addItem(new ItemStack(223));
 
 		for (int id = 201; id <= 386; id += 3)
