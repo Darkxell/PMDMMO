6fb8217ec6fe0a8e8b5ff05dae8978b0abf3ad51
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-10-27 10:52:22 +0200

    Overriding enables components

--- PMDMMO-DataEditor/src/fr/darkxell/dataeditor/application/controller/animation/EditVariantController.java
+++ PMDMMO-DataEditor/src/fr/darkxell/dataeditor/application/controller/animation/EditVariantController.java
@@ -1,6 +1,8 @@
 package fr.darkxell.dataeditor.application.controller.animation;
 
+import java.net.URL;
 import java.util.Arrays;
+import java.util.ResourceBundle;
 
 import com.darkxell.client.mechanics.animation.AnimationData;
 import com.darkxell.common.util.Direction;
@@ -8,6 +10,7 @@ import com.darkxell.common.util.Direction;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 public class EditVariantController extends EditAnimationController
 {
@@ -34,7 +37,7 @@ public class EditVariantController extends EditAnimationController
 	@FXML
 	public CheckBox overlayCheckbox;
 	@FXML
-	public CheckBox playsforeachtargetCheckbox;
+	public CheckBox playsforeachtargetValueCheckbox;
 	@FXML
 	public CheckBox pokemonMovementCheckbox;
 	@FXML
@@ -45,11 +48,167 @@ public class EditVariantController extends EditAnimationController
 	public CheckBox spriteDurationCheckbox;
 	@FXML
 	public CheckBox spritesCheckbox;
+	@FXML
+	public CheckBox stateDelayCheckbox;
 	public Direction variant;
 	@FXML
 	public CheckBox widthCheckbox, heightCheckbox;
 	@FXML
 	public CheckBox xCheckbox, yCheckbox;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources)
+	{
+		super.initialize(location, resources);
+
+		ToggleGroup group = new ToggleGroup();
+		this.hasStateRadio.setToggleGroup(group);
+		this.noStateRadio.setToggleGroup(group);
+
+		this.alsoplayTextfield.setDisable(true);
+		this.alsoplayDelayTextfield.setDisable(true);
+		this.animMovementCombobox.setDisable(true);
+		this.backspritesCombobox.setDisable(true);
+		this.clonesTextfield.setDisable(true);
+		this.delayTextfield.setDisable(true);
+		this.heightTextfield.setDisable(true);
+		this.loopTextfield.setDisable(true);
+		this.orderTextfield.setDisable(true);
+		this.overlayTextfield.setDisable(true);
+		this.playsforeachtargetValueCheckbox.setDisable(true);
+		this.pokemonMovementCombobox.setDisable(true);
+		this.soundDelayTextfield.setDisable(true);
+		this.soundTextfield.setDisable(true);
+		this.spriteDurationTextfield.setDisable(true);
+		this.spritesTextfield.setDisable(true);
+		this.customSpritesRadio.setDisable(true);
+		this.defaultSpritesRadio.setDisable(true);
+		this.noSpritesRadio.setDisable(true);
+		this.stateDelayTextfield.setDisable(true);
+		this.stateCombobox.setDisable(true);
+		this.hasStateRadio.setDisable(true);
+		this.noStateRadio.setDisable(true);
+		this.widthTextfield.setDisable(true);
+		this.xTextfield.setDisable(true);
+		this.yTextfield.setDisable(true);
+	}
+
+	public void onAlsoplayDelayOverride()
+	{
+		this.alsoplayDelayTextfield.setDisable(!this.alsoplayDelayCheckbox.isSelected());
+	}
+
+	public void onAlsoplayOverride()
+	{
+		this.alsoplayTextfield.setDisable(!this.alsoplayCheckbox.isSelected());
+	}
+
+	public void onAnimMovementOverride()
+	{
+		this.animMovementCombobox.setDisable(!this.animMovementCheckbox.isSelected());
+	}
+
+	public void onBackspritesOverride()
+	{
+		this.backspritesCombobox.setDisable(!this.backspritesCheckbox.isSelected());
+	}
+
+	public void onClonesOverride()
+	{
+		this.clonesTextfield.setDisable(!this.clonesCheckbox.isSelected());
+	}
+
+	public void onDelayOverride()
+	{
+		this.delayTextfield.setDisable(!this.delayCheckbox.isSelected());
+	}
+
+	public void onHeightOverride()
+	{
+		this.heightTextfield.setDisable(!this.heightCheckbox.isSelected());
+	}
+
+	public void onLoopOverride()
+	{
+		this.loopTextfield.setDisable(!this.loopCheckbox.isSelected());
+	}
+
+	public void onOrderOverride()
+	{
+		this.orderTextfield.setDisable(!this.orderCheckbox.isSelected());
+	}
+
+	public void onOverlayOverride()
+	{
+		this.overlayTextfield.setDisable(!this.overlayCheckbox.isSelected());
+	}
+
+	public void onPlaysforeachOverride()
+	{
+		this.playsforeachtargetValueCheckbox.setDisable(!this.playforeachtargetCheckbox.isSelected());
+	}
+
+	public void onPokemonMovementOverride()
+	{
+		this.pokemonMovementCombobox.setDisable(!this.pokemonMovementCheckbox.isSelected());
+	}
+
+	public void onSoundDelayOverride()
+	{
+		this.soundDelayTextfield.setDisable(!this.soundDelayCheckbox.isSelected());
+	}
+
+	public void onSoundOverride()
+	{
+		this.soundTextfield.setDisable(!this.soundCheckbox.isSelected());
+	}
+
+	public void onSpriteDurationOverride()
+	{
+		this.spriteDurationTextfield.setDisable(!this.spriteDurationCheckbox.isSelected());
+	}
+
+	public void onSpritesOverride()
+	{
+		boolean sprites = !this.spritesCheckbox.isSelected();
+		this.spritesTextfield.setDisable(sprites);
+		this.customSpritesRadio.setDisable(sprites);
+		this.defaultSpritesRadio.setDisable(sprites);
+		this.noSpritesRadio.setDisable(sprites);
+	}
+
+	@Override
+	public void onStateChange()
+	{
+		this.stateCombobox.setDisable(this.noStateRadio.isSelected());
+	}
+
+	public void onStateDelayOverride()
+	{
+		this.stateDelayTextfield.setDisable(!this.stateDelayCheckbox.isSelected());
+	}
+
+	public void onStateOverride()
+	{
+		this.stateCombobox.setDisable(!this.stateCheckbox.isSelected());
+		this.noStateRadio.setDisable(!this.stateCheckbox.isSelected());
+		this.hasStateRadio.setDisable(!this.stateCheckbox.isSelected());
+	}
+
+	public void onWidthOverride()
+	{
+		this.widthTextfield.setDisable(!this.widthCheckbox.isSelected());
+	}
+
+	public void onXOverride()
+	{
+		this.xTextfield.setDisable(!this.xCheckbox.isSelected());
+	}
+
+	public void onYOverride()
+	{
+		this.yTextfield.setDisable(!this.yCheckbox.isSelected());
+	}
 
 	public void setup(AnimationData variant)
 	{
@@ -65,6 +224,7 @@ public class EditVariantController extends EditAnimationController
 		this.orderCheckbox.setSelected(!Arrays.equals(variant.spriteOrder, this.data.spriteOrder));
 		this.overlayCheckbox.setSelected(variant.overlay != this.data.overlay);
 		this.playforeachtargetCheckbox.setSelected(variant.playsForEachTarget != this.data.playsForEachTarget);
+		this.playsforeachtargetValueCheckbox.setSelected(variant.playsForEachTarget);
 		this.pokemonMovementCheckbox.setSelected(
 				!((variant.pokemonMovement == null && this.data.pokemonMovement == null) || variant.pokemonMovement.equals(this.data.pokemonMovement)));
 		this.soundCheckbox.setSelected(!((variant.sound == null && this.data.sound == null) || variant.sound.equals(this.data.sound)));
