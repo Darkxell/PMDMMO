8e9f6de4c22b9fb271ca619ee0c4816a8efe1cfd
CANDELA Nicolas <darkxell.mc@gmail.com>
2018-11-09 16:44:42 +0100

    Added storyadvance to tinywoods fail cutscene

--- PMDMMOc/src/com/darkxell/client/mechanics/cutscene/end/ArbitraryCutsceneEnds.java
+++ PMDMMOc/src/com/darkxell/client/mechanics/cutscene/end/ArbitraryCutsceneEnds.java
@@ -1,8 +1,11 @@
 package com.darkxell.client.mechanics.cutscene.end;
 
+import com.darkxell.client.launchable.Persistance;
 import com.darkxell.client.mechanics.cutscene.Cutscene;
 import com.darkxell.client.mechanics.cutscene.end.arbitrary.StartNameAsk;
 import com.darkxell.common.util.Logger;
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
 
 public class ArbitraryCutsceneEnds {
 
@@ -14,6 +17,10 @@ public class ArbitraryCutsceneEnds {
 		switch (function) {
 		case "startnameask":
 			StartNameAsk.startNameAsk();
+			break;
+		case "enterwoods":
+			JsonObject mess = Json.object().add("action", "storyadvance").add("target", 2);
+			Persistance.socketendpoint.sendMessage(mess.toString());
 			break;
 		default:
 			defaultFunction(function, cutscene);
