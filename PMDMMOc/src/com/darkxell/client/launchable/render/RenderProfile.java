d85677dff0fe81f0fd5bd0b4588f898f6418c789
Shane Duan <s_duan@u.pacific.edu>
2018-12-13 00:28:24 -0800

    Fix enum formatting config

--- PMDMMOc/src/com/darkxell/client/launchable/render/RenderProfile.java
+++ PMDMMOc/src/com/darkxell/client/launchable/render/RenderProfile.java
@@ -3,10 +3,11 @@ package com.darkxell.client.launchable.render;
 /**
  * Marker for different kinds of render and update loops.
  */
-public enum RenderProfile {/**
- * Synchronized render loop. Should be default.
- */
-PROFILE_SYNCHRONIZED,
+public enum RenderProfile {
+    /**
+     * Synchronized render loop. Should be default.
+     */
+    PROFILE_SYNCHRONIZED,
     /**
      * Render and update are in separate threads. Do not use at the moment - gets really choppy!
      */
