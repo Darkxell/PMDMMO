bc02d604bc0861290ffbefae1c4ae2415977c816
Shane Duan <s_duan@u.pacific.edu>
2018-12-14 07:14:44 -0800

    Lint Lang

--- PMDMMO-common/src/com/darkxell/common/util/language/Lang.java
+++ PMDMMO-common/src/com/darkxell/common/util/language/Lang.java
@@ -5,76 +5,69 @@ import java.util.Properties;
 
 import com.darkxell.common.util.Logger;
 
-public class Lang
-{
+public class Lang {
+    public enum Language {
+        ENGLISH("en", "English");
 
-	public static enum Language
-	{
-		ENGLISH("en", "English");
+        public final String id;
+        public final String name;
 
-		public final String id;
-		public final String name;
+        Language(String id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
 
-		private Language(String id, String name)
-		{
-			this.id = id;
-			this.name = name;
-		}
-	}
+    private static Properties dictionnary = new Properties();
+    private static Language selected;
 
-	private static Properties dictionnary = new Properties();
-	private static Language selected;
+    public static boolean containsKey(String id) {
+        return dictionnary.containsKey(id);
+    }
 
-	public static boolean containsKey(String id)
-	{
-		return dictionnary.containsKey(id);
-	}
+    public static Language getLanguage() {
+        return selected;
+    }
 
-	public static Language getLanguage()
-	{
-		return selected;
-	}
+    public static void load(boolean forceLoad) {
+        setLanguage(Language.ENGLISH);
+        if (forceLoad) {
+            updateTranslations();
+            Keywords.updateKeywords();
+        }
+    }
 
-	public static void load(boolean forceLoad)
-	{
-		setLanguage(Language.ENGLISH);
-		if (forceLoad)
-		{
-			updateTranslations();
-			Keywords.updateKeywords();
-		}
-	}
+    public static void setLanguage(Language language) {
+        if (language == selected) {
+            return;
+        }
+        Logger.instance().info("Language set to " + language.name + ".");
+        selected = language;
+        updateTranslations();
+        Keywords.updateKeywords();
+    }
 
-	public static void setLanguage(Language language)
-	{
-		if (language == selected) return;
-		Logger.instance().info("Language set to " + language.name + ".");
-		selected = language;
-		updateTranslations();
-		Keywords.updateKeywords();
-	}
+    public static String translate(String id) {
+        if (dictionnary.containsKey(id)) {
+            return dictionnary.getProperty(id);
+        }
+        return id;
+    }
 
-	public static String translate(String id)
-	{
-		if (dictionnary.containsKey(id)) return dictionnary.getProperty(id);
-		return id;
-	}
-
-	/** Reloads the translations after switching language. */
-	private static void updateTranslations()
-	{
-		dictionnary.clear();
-		try
-		{
-			InputStream stream = Lang.class.getResourceAsStream("/lang/" + selected.id + ".properties");
-			if (stream != null) dictionnary.load(stream);
-		} catch (FileNotFoundException e)
-		{
-			e.printStackTrace();
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
+    /**
+     * Reloads the translations after switching language.
+     */
+    private static void updateTranslations() {
+        dictionnary.clear();
+        try {
+            InputStream stream = Lang.class.getResourceAsStream("/lang/" + selected.id + ".properties");
+            if (stream != null) {
+                dictionnary.load(stream);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
