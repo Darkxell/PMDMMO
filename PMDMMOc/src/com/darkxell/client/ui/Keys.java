8cd1ecef2f7f45af5fa873f478efd8fb5f037a3c
Shane Duan <s_duan@u.pacific.edu>
2018-12-15 10:44:12 -0800

    Remove redundant Key field qualifiers

--- PMDMMOc/src/com/darkxell/client/ui/Keys.java
+++ PMDMMOc/src/com/darkxell/client/ui/Keys.java
@@ -11,8 +11,7 @@ import com.darkxell.common.util.Logger;
 import com.darkxell.common.util.language.Message;
 
 public class Keys implements KeyListener {
-
-	public static enum Key {
+	public enum Key {
 		ATTACK("attack"),
 		DIAGONAL("diagonal"),
 		DOWN("down"),
@@ -45,7 +44,8 @@ public class Keys implements KeyListener {
 		boolean wasPressed;
 		boolean willPress;
 
-		private Key(String nameID) {
+		Key(String nameID)
+		{
 			this.nameID = nameID;
 		}
 
@@ -73,7 +73,7 @@ public class Keys implements KeyListener {
 	/** Checks if the input directional keys are pressed. <br />
 	 * If the RUN key is pressed, will check if they were'nt pressed the last tick.<br />
 	 * Else, will check if they're the only directional keys pressed.
-	 * 
+	 *
 	 * @param canRun - True if the RUN key should be checked.
 	 * @param keys - The keys to check. */
 	public static boolean directionPressed(boolean canRun, Key... keys) {
