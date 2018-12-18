d7f3b8c721342e4633f03c45c074a18734b67fb4
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-12-18 13:30:51 +0100

    Prevent crash on load null Cutscene

--- PMDMMOc/src/com/darkxell/client/mechanics/cutscene/CutsceneManager.java
+++ PMDMMOc/src/com/darkxell/client/mechanics/cutscene/CutsceneManager.java
@@ -1,5 +1,7 @@
 package com.darkxell.client.mechanics.cutscene;
 
+import java.io.InputStream;
+
 import com.darkxell.client.launchable.Persistance;
 import com.darkxell.client.resources.Res;
 import com.darkxell.client.state.TransitionState;
@@ -11,12 +13,15 @@ public class CutsceneManager
 
 	public static Cutscene loadCutscene(String id)
 	{
-		return new Cutscene(id, XMLUtils.read(Res.get("/cutscenes/" + id + ".xml")));
+		InputStream is = Res.get("/cutscenes/" + id + ".xml");
+		if (is == null) return null;
+		return new Cutscene(id, XMLUtils.read(is));
 	}
 
 	public static void playCutscene(String id, boolean fading)
 	{
 		Cutscene c = loadCutscene(id);
+		if (c == null) return;
 		Persistance.cutsceneState = new CutsceneState(c);
 
 		if (fading) Persistance.stateManager.setState(new TransitionState(Persistance.stateManager.getCurrentState(), Persistance.cutsceneState) {
