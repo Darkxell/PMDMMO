b31ac6a4daafc5b4fed751e1194965e8c59bc670
Shane Duan <s_duan@u.pacific.edu>
2018-12-18 16:43:19 -0800

    Lint documentation of freezone camera, trigger zone, and warp zones

--- PMDMMOc/src/com/darkxell/client/mechanics/freezones/entities/FreezoneCamera.java
+++ PMDMMOc/src/com/darkxell/client/mechanics/freezones/entities/FreezoneCamera.java
@@ -3,18 +3,21 @@ package com.darkxell.client.mechanics.freezones.entities;
 import com.darkxell.client.launchable.Persistence;
 import com.darkxell.client.mechanics.freezones.FreezonePlayer;
 
-/** The camera "entity". This behaves pretty much exactly like an entity, but is not rendered and doesn't update as the others do. This entity will follow the player as much as it can, but will avoid showing out of bounds areas if the player is too close to walls. Also this entity, like tha player,
- * is not dependennt to a map and will follow the player through warpzone by being persistant. */
+/**
+ * Pseudo-entity that corresponds to the camera position.
+ *
+ * This entity will attempt to track the player, but will avoid showing positions that are out-of-bounds. This entity
+ * is persistent across all freezone maps.
+ */
 public class FreezoneCamera {
-
-	private static final int SHAKETIME = 5;
-	private static final int TILESIZE = 8;
+    private static final int SHAKETIME = 5;
+    private static final int TILESIZE = 8;
     public int renderheight = -1;
     public int renderwidth = -1;
-	private int shakeTimer = 0;
-	private int shakeX = 0, shakeY = 0;
-	private int shaking = 0;
-	private FreezonePlayer target;
+    private int shakeTimer = 0;
+    private int shakeX = 0, shakeY = 0;
+    private int shaking = 0;
+    private FreezonePlayer target;
     private double x = 0;
     private double y = 0;
 
@@ -34,39 +37,38 @@ public class FreezoneCamera {
         this.y = y;
     }
 
-	/** Creates a new Freezonecamera following the wanted player. (Don't go and create multiple players just for the sake of adding multiple cameras tho...) */
-	public FreezoneCamera(FreezonePlayer tofollow) {
-		this.target = tofollow;
-		if (tofollow != null) {
-			this.x = tofollow.x;
-			this.y = tofollow.y;
-		}
-	}
+    public FreezoneCamera(FreezonePlayer targetPlayer) {
+        this.target = targetPlayer;
+        if (targetPlayer != null) {
+            this.x = targetPlayer.x;
+            this.y = targetPlayer.y;
+        }
+    }
 
-	public double finalX() {
-		return this.x + this.shakeX;
-	}
+    public double finalX() {
+        return this.x + this.shakeX;
+    }
 
-	public double finalY() {
+    public double finalY() {
         return this.y + this.shakeY;
     }
 
     private void onShakeTick() {
         if (this.shakeX == 0 && this.shakeY == 0) {
             this.shakeX = this.shakeY = this.shaking;
-		} else if (this.shakeX > 0 && this.shakeY > 0) {
-			this.shakeX = -this.shaking;
-			this.shakeY = this.shaking;
-		} else if (this.shakeX < 0 && this.shakeY > 0) {
-			this.shakeX = -this.shaking;
-			this.shakeY = -this.shaking;
-		} else if (this.shakeX < 0 && this.shakeY < 0) {
-			this.shakeX = this.shaking;
-			this.shakeY = -this.shaking;
-		} else if (this.shakeX > 0 && this.shakeY < 0) {
-			this.shakeX = this.shakeY = 0;
-		}
-	}
+        } else if (this.shakeX > 0 && this.shakeY > 0) {
+            this.shakeX = -this.shaking;
+            this.shakeY = this.shaking;
+        } else if (this.shakeX < 0 && this.shakeY > 0) {
+            this.shakeX = -this.shaking;
+            this.shakeY = -this.shaking;
+        } else if (this.shakeX < 0 && this.shakeY < 0) {
+            this.shakeX = this.shaking;
+            this.shakeY = -this.shaking;
+        } else if (this.shakeX > 0 && this.shakeY < 0) {
+            this.shakeX = this.shakeY = 0;
+        }
+    }
 
     public void setShaking(int strength) {
         this.shaking = strength;
@@ -108,15 +110,15 @@ public class FreezoneCamera {
         double minPos = maxTiles / 2 + 0.3;
         double maxPos = mapTiles - maxTiles / 2 - 0.3;
         return Math.max(minPos, Math.min(maxPos, val));
-	}
+    }
 
-	public void update() {
-		// Shake
-		if (this.shaking != 0) {
-			++this.shakeTimer;
-			if (this.shakeTimer >= SHAKETIME) {
-				this.shakeTimer = 0;
-				this.onShakeTick();
+    public void update() {
+        // Shake
+        if (this.shaking != 0) {
+            ++this.shakeTimer;
+            if (this.shakeTimer >= SHAKETIME) {
+                this.shakeTimer = 0;
+                this.onShakeTick();
             }
         }
 
