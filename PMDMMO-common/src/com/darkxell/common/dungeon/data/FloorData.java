ffa9b0307122cdf251a2558bcd66badd47dce71e
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-12-30 18:40:22 +0100

    Defeated boss cutscene

--- PMDMMO-common/src/com/darkxell/common/dungeon/data/FloorData.java
+++ PMDMMO-common/src/com/darkxell/common/dungeon/data/FloorData.java
@@ -8,14 +8,15 @@ import com.darkxell.common.move.Move;
 import com.darkxell.common.pokemon.PokemonType;
 import com.darkxell.common.util.XMLUtils;
 
-public class FloorData implements Comparable<FloorData>
-{
+public class FloorData implements Comparable<FloorData> {
 
 	public static final byte NO_SHADOW = 0, NORMAL_SHADOW = 1, DENSE_SHADOW = 2;
 	public static final String XML_ROOT = "d";
 
 	/** The base Money of the Floor. Used to generate piles of Pokedollars. */
 	private int baseMoney;
+	/** If not -1, the storypos index required to make this a bossfight. If not matched, this floor will only play a cutscene. */
+	private int bossFloor = -1;
 	/** The density of Buried Items. */
 	private short buriedItemDensity;
 	/** The type given to a Pokemon using the move Camouflage. */
@@ -24,8 +25,6 @@ public class FloorData implements Comparable<FloorData>
 	private int difficulty;
 	/** Describes which Floors this Data applies to. This should always be a continuous single set of floors [start - end]. */
 	private FloorSet floors;
-	/** True if this is a Boss Floor. */
-	private boolean isBossFloor = false;
 	/** The density of Items. */
 	private short itemDensity;
 	/** The Layout to use to generate the Floor. */
@@ -49,15 +48,14 @@ public class FloorData implements Comparable<FloorData>
 	/** The density of Traps. */
 	private short trapDensity;
 
-	public FloorData(Element xml)
-	{
+	public FloorData(Element xml) {
 		this.load(xml);
 	}
 
-	public FloorData(FloorSet floors, int difficulty, int baseMoney, int layout, int terrainSpriteset, byte shadows, PokemonType camouflageType,
-			int naturePower, String secretPower, int soundtrack, short shopChance, short monsterHouseChance, short itemDensity, short pokemonDensity,
-			short trapDensity, short buriedItemDensity, boolean isBossFloor)
-	{
+	public FloorData(FloorSet floors, int difficulty, int baseMoney, int layout, int terrainSpriteset, byte shadows,
+			PokemonType camouflageType, int naturePower, String secretPower, int soundtrack, short shopChance,
+			short monsterHouseChance, short itemDensity, short pokemonDensity, short trapDensity,
+			short buriedItemDensity, int bossFloor) {
 		this.floors = floors;
 		this.difficulty = difficulty;
 		this.baseMoney = baseMoney;
@@ -74,82 +72,75 @@ public class FloorData implements Comparable<FloorData>
 		this.pokemonDensity = pokemonDensity;
 		this.trapDensity = trapDensity;
 		this.buriedItemDensity = buriedItemDensity;
-		this.isBossFloor = isBossFloor;
+		this.bossFloor = bossFloor;
 	}
 
-	public int baseMoney()
-	{
+	public int baseMoney() {
 		return this.baseMoney;
 	}
 
-	public short buriedItemDensity()
-	{
+	public int bossFloor() {
+		return this.bossFloor;
+	}
+
+	public short buriedItemDensity() {
 		return this.buriedItemDensity;
 	}
 
-	public PokemonType camouflageType()
-	{
+	public PokemonType camouflageType() {
 		return this.camouflageType;
 	}
 
 	@Override
-	public int compareTo(FloorData o)
-	{
+	public int compareTo(FloorData o) {
 		return this.floors.compareTo(o.floors);
 	}
 
 	/** @return A copy of this Data. */
-	public FloorData copy()
-	{
-		return new FloorData(this.floors.copy(), this.difficulty, this.baseMoney, this.layout, this.terrainSpriteset, this.shadows, this.camouflageType,
-				this.naturePower, this.secretPower, this.soundtrack, this.shopChance, this.monsterHouseChance, this.itemDensity, this.pokemonDensity,
-				this.trapDensity, this.buriedItemDensity, this.isBossFloor);
+	public FloorData copy() {
+		return new FloorData(this.floors.copy(), this.difficulty, this.baseMoney, this.layout, this.terrainSpriteset,
+				this.shadows, this.camouflageType, this.naturePower, this.secretPower, this.soundtrack, this.shopChance,
+				this.monsterHouseChance, this.itemDensity, this.pokemonDensity, this.trapDensity,
+				this.buriedItemDensity, this.bossFloor);
 	}
 
-	public int difficulty()
-	{
+	public int difficulty() {
 		return this.difficulty;
 	}
 
-	public FloorSet floors()
-	{
+	public FloorSet floors() {
 		return this.floors;
 	}
 
-	public boolean hasCustomTileset()
-	{
+	public boolean hasCustomTileset() {
 		return this.terrainSpriteset() == -1;
 	}
 
-	public boolean isBossFloor()
-	{
-		return this.isBossFloor;
+	public boolean isBossFloor() {
+		return this.bossFloor != -1;
 	}
 
-	public short itemDensity()
-	{
+	public short itemDensity() {
 		return this.itemDensity;
 	}
 
-	public Layout layout()
-	{
+	public Layout layout() {
 		return Layout.find(this.layout);
 	}
 
-	public int layoutID()
-	{
+	public int layoutID() {
 		return this.layout;
 	}
 
-	public void load(Element xml)
-	{
+	public void load(Element xml) {
 		this.floors = new FloorSet(xml.getChild(FloorSet.XML_ROOT, xml.getNamespace()));
 		this.difficulty = XMLUtils.getAttribute(xml, "difficulty", this.difficulty);
 		this.baseMoney = XMLUtils.getAttribute(xml, "money", this.baseMoney);
 		this.layout = XMLUtils.getAttribute(xml, "layout", this.layout);
 		this.terrainSpriteset = XMLUtils.getAttribute(xml, "terrain", this.terrainSpriteset);
 		this.shadows = XMLUtils.getAttribute(xml, "shadows", this.shadows);
-		this.camouflageType = PokemonType.find(XMLUtils.getAttribute(xml, "camouflage", this.camouflageType == null ? 0 : this.camouflageType.id));
+		this.camouflageType = PokemonType.find(
+				XMLUtils.getAttribute(xml, "camouflage", this.camouflageType == null ? 0 : this.camouflageType.id));
 		this.naturePower = XMLUtils.getAttribute(xml, "nature", this.naturePower);
 		this.secretPower = xml.getAttributeValue("secret");
 		this.soundtrack = XMLUtils.getAttribute(xml, "soundtrack", this.soundtrack);
@@ -159,90 +150,88 @@ public class FloorData implements Comparable<FloorData>
 		this.pokemonDensity = XMLUtils.getAttribute(xml, "pokemon", this.pokemonDensity);
 		this.trapDensity = XMLUtils.getAttribute(xml, "traps", this.trapDensity);
 		this.buriedItemDensity = XMLUtils.getAttribute(xml, "buried", this.buriedItemDensity);
-		this.isBossFloor = XMLUtils.getAttribute(xml, "bossfloor", this.isBossFloor);
+		this.bossFloor = XMLUtils.getAttribute(xml, "bossfloor", this.bossFloor);
 	}
 
-	public short monsterHouseChance()
-	{
+	public short monsterHouseChance() {
 		return this.monsterHouseChance;
 	}
 
-	public Move naturePower()
-	{
+	public Move naturePower() {
 		return Registries.moves().find(this.naturePower);
 	}
 
-	public int naturePowerID()
-	{
+	public int naturePowerID() {
 		return this.naturePower;
 	}
 
-	public short pokemonDensity()
-	{
+	public short pokemonDensity() {
 		return this.pokemonDensity;
 	}
 
-	public String secretPower()
-	{
+	public String secretPower() {
 		return this.secretPower;
 	}
 
-	public byte shadows()
-	{
+	public byte shadows() {
 		return this.shadows;
 	}
 
-	public short shopChance()
-	{
+	public short shopChance() {
 		return this.shopChance;
 	}
 
-	public int soundtrack()
-	{
+	public int soundtrack() {
 		return this.soundtrack;
 	}
 
-	public int terrainSpriteset()
-	{
+	public int terrainSpriteset() {
 		return this.terrainSpriteset;
 	}
 
-	public Element toXML(FloorData previous)
-	{
+	public Element toXML(FloorData previous) {
 		Element xml = new Element(XML_ROOT);
 		xml.addContent(this.floors.toXML());
-		if (previous != null || this.difficulty != 0) XMLUtils.setAttribute(xml, "difficulty", this.difficulty, previous != null ? previous.difficulty : -1);
-		if (previous != null || this.baseMoney != 0) XMLUtils.setAttribute(xml, "money", this.baseMoney, previous != null ? previous.baseMoney : -1);
-		if (previous != null || this.layout != 0) XMLUtils.setAttribute(xml, "layout", this.layout, previous != null ? previous.layout : -1);
-		if (previous != null || this.terrainSpriteset != 0)
-			XMLUtils.setAttribute(xml, "terrain", this.terrainSpriteset, previous != null ? previous.terrainSpriteset : -1);
-		if (previous != null || this.shadows != 0) XMLUtils.setAttribute(xml, "shadows", this.shadows, previous != null ? previous.shadows : -1);
-		if (previous != null || this.camouflageType.id != 0)
-			XMLUtils.setAttribute(xml, "camouflage", this.camouflageType.id, previous != null ? previous.camouflageType.id : -1);
-		if (previous != null || this.naturePower != 0) XMLUtils.setAttribute(xml, "nature", this.naturePower, previous != null ? previous.naturePower : -1);
+		if (previous != null || this.difficulty != 0)
+			XMLUtils.setAttribute(xml, "difficulty", this.difficulty, previous != null ? previous.difficulty : -1);
+		if (previous != null || this.baseMoney != 0)
+			XMLUtils.setAttribute(xml, "money", this.baseMoney, previous != null ? previous.baseMoney : -1);
+		if (previous != null || this.layout != 0)
+			XMLUtils.setAttribute(xml, "layout", this.layout, previous != null ? previous.layout : -1);
+		if (previous != null || this.terrainSpriteset != 0) XMLUtils.setAttribute(xml, "terrain", this.terrainSpriteset,
+				previous != null ? previous.terrainSpriteset : -1);
+		if (previous != null || this.shadows != 0)
+			XMLUtils.setAttribute(xml, "shadows", this.shadows, previous != null ? previous.shadows : -1);
+		if (previous != null || this.camouflageType.id != 0) XMLUtils.setAttribute(xml, "camouflage",
+				this.camouflageType.id, previous != null ? previous.camouflageType.id : -1);
+		if (previous != null || this.naturePower != 0)
+			XMLUtils.setAttribute(xml, "nature", this.naturePower, previous != null ? previous.naturePower : -1);
 		if (previous != null || this.secretPower != null)
 			XMLUtils.setAttribute(xml, "secret", this.secretPower, previous != null ? previous.secretPower : null);
-		if (previous != null || this.soundtrack != 0) XMLUtils.setAttribute(xml, "soundtrack", this.soundtrack, previous != null ? previous.soundtrack : 0);
-		if (previous != null || this.shopChance != 0) XMLUtils.setAttribute(xml, "shop", this.shopChance, previous != null ? previous.shopChance : -1);
-		if (previous != null || this.monsterHouseChance != 0)
-			XMLUtils.setAttribute(xml, "mhouse", this.monsterHouseChance, previous != null ? previous.monsterHouseChance : -1);
-		if (previous != null || this.itemDensity != 0) XMLUtils.setAttribute(xml, "items", this.itemDensity, previous != null ? previous.itemDensity : -1);
+		if (previous != null || this.soundtrack != 0)
+			XMLUtils.setAttribute(xml, "soundtrack", this.soundtrack, previous != null ? previous.soundtrack : 0);
+		if (previous != null || this.shopChance != 0)
+			XMLUtils.setAttribute(xml, "shop", this.shopChance, previous != null ? previous.shopChance : -1);
+		if (previous != null || this.monsterHouseChance != 0) XMLUtils.setAttribute(xml, "mhouse",
+				this.monsterHouseChance, previous != null ? previous.monsterHouseChance : -1);
+		if (previous != null || this.itemDensity != 0)
+			XMLUtils.setAttribute(xml, "items", this.itemDensity, previous != null ? previous.itemDensity : -1);
 		if (previous != null || this.pokemonDensity != 0)
 			XMLUtils.setAttribute(xml, "pokemon", this.pokemonDensity, previous != null ? previous.pokemonDensity : -1);
-		if (previous != null || this.trapDensity != 0) XMLUtils.setAttribute(xml, "traps", this.trapDensity, previous != null ? previous.trapDensity : -1);
-		if (previous != null || this.buriedItemDensity != 0)
-			XMLUtils.setAttribute(xml, "buried", this.buriedItemDensity, previous != null ? previous.buriedItemDensity : -1);
-		if (previous != null || !this.isBossFloor) XMLUtils.setAttribute(xml, "bossfloor", this.isBossFloor, previous != null ? previous.isBossFloor : false);
+		if (previous != null || this.trapDensity != 0)
+			XMLUtils.setAttribute(xml, "traps", this.trapDensity, previous != null ? previous.trapDensity : -1);
+		if (previous != null || this.buriedItemDensity != 0) XMLUtils.setAttribute(xml, "buried",
+				this.buriedItemDensity, previous != null ? previous.buriedItemDensity : -1);
+		if (previous != null || this.bossFloor != -1)
+			XMLUtils.setAttribute(xml, "bossfloor", this.bossFloor, previous != null ? previous.bossFloor : -1);
 		return xml;
 	}
 
-	public short trapDensity()
-	{
+	public short trapDensity() {
 		return this.trapDensity;
 	}
 
-	public int visionDistance()
-	{
+	public int visionDistance() {
 		if (this.shadows == DENSE_SHADOW) return 1;
 		if (this.shadows == NO_SHADOW) return 2;
 		return 5;
