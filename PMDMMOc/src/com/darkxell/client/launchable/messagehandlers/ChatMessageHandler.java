8093be5f04c5448be73e23285bdc318dca9abc08
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2019-01-04 16:10:19 +0100

    Generated deploykeys are copied to clipboard

--- PMDMMOc/src/com/darkxell/client/launchable/messagehandlers/ChatMessageHandler.java
+++ PMDMMOc/src/com/darkxell/client/launchable/messagehandlers/ChatMessageHandler.java
@@ -1,5 +1,9 @@
 package com.darkxell.client.launchable.messagehandlers;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
 import com.darkxell.client.launchable.Persistence;
 import com.darkxell.client.mechanics.chat.ChatMessage;
 import com.darkxell.common.util.Logger;
@@ -9,9 +13,17 @@ public class ChatMessageHandler extends MessageHandler {
 
 	@Override
 	public void handleMessage(JsonObject message) {
+
+		ChatMessage cm = new ChatMessage(message);
+		if (cm.message.contains("A new deploy key has been generated:") && cm.sender.equals("Help")) {
+			StringSelection selection = new StringSelection(cm.message);
+			Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+			clipboard.setContents(selection, selection);
+		}
+
 		try {
 			if (Persistence.chatbox != null && Persistence.chatbox.messages != null)
-				Persistence.chatbox.messages.add(new ChatMessage(message));
+				Persistence.chatbox.messages.add(cm);
 			else {
 				Logger.w("Couldn't write the following message in chat (not yet loaded) : " + message);
 			}
