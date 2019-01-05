8e5ac62f6b1f97840ce2ede556a1581ddb838c9e
Shane Duan <s_duan@u.pacific.edu>
2019-01-04 20:57:18 -0800

    Add powershell launch script

--- launcher/static/setup.ps1
+++ launcher/static/setup.ps1
@@ -1,2 +1,25 @@
-java -version
-Start-Sleep 1
+function Download-File {
+    param(
+        [String] $url,
+        [String] $path
+    )
+
+    $client = New-Object System.Net.WebClient
+    $client.DownloadFile($url, $path)
+}
+
+$java_info = Get-Command java -errorAction SilentlyContinue
+if (!$java_info -or ($java_info.Version.Major -lt 8)) {
+    exit 2 # "jre not installed"
+}
+
+# exit 3 # "could not connect to server"
+# TODO: install (& verify?) jar
+# exit 4 # "could not verify download"
+# TODO: install data files
+# exit 5 # "could not get game data"
+
+Write-Host "Successfully installed!"
+
+Start-Process 'D:\git\PMDMMO\out\artifacts\PMDMMOc_jar\PMDMMOc.jar'
+    -NoNewWindow
