d459eb023b4bc9f0c88304f66ab4ed763be0ef47
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2019-01-13 12:47:39 +0100

    Ability: 33- Effect Spore

---
+++ PMDMMO-common/src/com/darkxell/common/pokemon/ability/AbilityEffectSpore.java
@@ -0,0 +1,48 @@
+package com.darkxell.common.pokemon.ability;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.darkxell.common.dungeon.floor.Floor;
+import com.darkxell.common.event.DungeonEvent;
+import com.darkxell.common.event.move.MoveSelectionEvent.MoveUse;
+import com.darkxell.common.event.pokemon.DamageDealtEvent;
+import com.darkxell.common.event.pokemon.StatusConditionCreatedEvent;
+import com.darkxell.common.event.pokemon.TriggeredAbilityEvent;
+import com.darkxell.common.pokemon.DungeonPokemon;
+import com.darkxell.common.pokemon.PokemonType;
+import com.darkxell.common.status.StatusCondition;
+import com.darkxell.common.status.StatusConditions;
+import com.darkxell.common.util.RandomUtil;
+
+public class AbilityEffectSpore extends AbilityOnHit {
+
+	public final StatusCondition[] conditions;
+
+	public AbilityEffectSpore(int id) {
+		super(id, 12);
+		this.conditions = new StatusCondition[] { StatusConditions.Paralyzed, StatusConditions.Asleep,
+				StatusConditions.Poisoned };
+	}
+
+	private boolean isImmune(Floor floor, DungeonPokemon user) {
+		return user.species().isType(PokemonType.Grass);
+	}
+
+	@Override
+	protected void onHit(Floor floor, DamageDealtEvent event, MoveUse source, TriggeredAbilityEvent abilityEvent,
+			ArrayList<DungeonEvent> resultingEvents) {
+		if (!this.isImmune(floor, source.user)) {
+			StatusCondition condition = RandomUtil.random(Arrays.asList(this.conditions), floor.random);
+
+			for (int i = 0; i < this.conditions.length; ++i)
+				if (this.conditions[i] == condition) {
+					abilityEvent.messageID = i + 1;
+					break;
+				}
+
+			resultingEvents.add(new StatusConditionCreatedEvent(floor,
+					condition.create(source.user, abilityEvent.pokemon, floor.random)));
+		}
+	}
+}
