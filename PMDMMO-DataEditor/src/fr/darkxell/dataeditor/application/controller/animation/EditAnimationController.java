d72c7cc6511112e765cc1d7aaa6a643075fb9a2c
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2019-01-26 20:58:53 +0100

    Move: 63- Supersonic

--- PMDMMO-DataEditor/src/fr/darkxell/dataeditor/application/controller/animation/EditAnimationController.java
+++ PMDMMO-DataEditor/src/fr/darkxell/dataeditor/application/controller/animation/EditAnimationController.java
@@ -28,8 +28,7 @@ import javafx.scene.control.TextFormatter;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
-public class EditAnimationController implements Initializable
-{
+public class EditAnimationController implements Initializable {
 
 	public static EditAnimationController instance;
 	public static Stage variantPopup;
@@ -78,8 +77,7 @@ public class EditAnimationController implements Initializable
 	public TextField xTextfield, yTextfield;
 
 	@Override
-	public void initialize(URL location, ResourceBundle resources)
-	{
+	public void initialize(URL location, ResourceBundle resources) {
 		instance = this;
 
 		ToggleGroup spritesGroup = new ToggleGroup();
@@ -101,7 +99,8 @@ public class EditAnimationController implements Initializable
 		this.pokemonMovementCombobox.getItems().addAll("none", "dash", "2tiles", "smalljump");
 		this.pokemonMovementCombobox.getSelectionModel().select(0);
 
-		this.animMovementCombobox.getItems().addAll("none", "1tilefacing", "diagonal", "upanddown", "straight (projectile)", "arc (projectile)");
+		this.animMovementCombobox.getItems().addAll("none", "1tilefacing", "diagonal", "upanddown",
+				"straight (projectile)", "arc (projectile)");
 		this.animMovementCombobox.getSelectionModel().select(0);
 
 		this.stateCheckbox.selectedProperty().addListener((v, nv, ov) -> this.onStateChange());
@@ -121,9 +120,10 @@ public class EditAnimationController implements Initializable
 		this.soundDelayTextfield.setTextFormatter(new TextFormatter<>((UnaryOperator<TextFormatter.Change>) change -> {
 			return pattern.matcher(change.getControlNewText()).matches() ? change : null;
 		}));
-		this.spriteDurationTextfield.setTextFormatter(new TextFormatter<>((UnaryOperator<TextFormatter.Change>) change -> {
-			return pattern.matcher(change.getControlNewText()).matches() ? change : null;
-		}));
+		this.spriteDurationTextfield
+				.setTextFormatter(new TextFormatter<>((UnaryOperator<TextFormatter.Change>) change -> {
+					return pattern.matcher(change.getControlNewText()).matches() ? change : null;
+				}));
 		this.stateDelayTextfield.setTextFormatter(new TextFormatter<>((UnaryOperator<TextFormatter.Change>) change -> {
 			return pattern.matcher(change.getControlNewText()).matches() ? change : null;
 		}));
@@ -141,93 +141,80 @@ public class EditAnimationController implements Initializable
 		}));
 
 		Pattern p2 = Pattern.compile("(\\d+,)*(\\d+)?");
-		this.alsoplayDelayTextfield.setTextFormatter(new TextFormatter<>((UnaryOperator<TextFormatter.Change>) change -> {
-			return p2.matcher(change.getControlNewText()).matches() ? change : null;
-		}));
+		this.alsoplayDelayTextfield
+				.setTextFormatter(new TextFormatter<>((UnaryOperator<TextFormatter.Change>) change -> {
+					return p2.matcher(change.getControlNewText()).matches() ? change : null;
+				}));
 		Pattern p3 = Pattern.compile("(-?\\d+,)*(-?\\d+)?");
 		this.orderTextfield.setTextFormatter(new TextFormatter<>((UnaryOperator<TextFormatter.Change>) change -> {
 			return p3.matcher(change.getControlNewText()).matches() ? change : null;
 		}));
 	}
 
-	public void onCancelChanges()
-	{
+	public void onCancelChanges() {
 		this.setupFor(AnimationsTabController.instance.editing);
 	}
 
-	public void onChangeID()
-	{
+	public void onChangeID() {
 		AnimationsTabController.instance.onChangeID();
 	}
 
-	public void onEditVariant()
-	{
+	public void onEditVariant() {
 		FXMLLoader loader = new FXMLLoader(DataEditor.class.getResource("/layouts/animations/select_variant.fxml"));
-		try
-		{
+		try {
 			Parent root = loader.load();
 			variantPopup = FXUtils.showPopup(root, "Choose variant to edit");
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void onEditVariant(Direction direction)
-	{
-		try
-		{
+	public void onEditVariant(Direction direction) {
+		try {
 			FXMLLoader loader = new FXMLLoader(DataEditor.class.getResource("/layouts/animations/edit_variant.fxml"));
 			Parent root = loader.load();
-			if (AnimationsTabController.instance.editing != null)
-			{
-				AnimationData data = Animations.getData(AnimationsTabController.instance.editing.id, AnimationsTabController.instance.editing.group);
+			if (AnimationsTabController.instance.editing != null) {
+				AnimationData data = Animations.getData(AnimationsTabController.instance.editing.id,
+						AnimationsTabController.instance.editing.group);
 				EditVariantController controller = loader.getController();
 				controller.data = data;
 				controller.variant = direction;
 				controller.setup(data.getVariant(direction));
 				variantPopup = FXUtils.showPopup(root, "Edit Variant: " + direction);
 			}
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void onSave()
-	{
-		AnimationData anim = Animations.getData(AnimationsTabController.instance.editing.id, AnimationsTabController.instance.editing.group);
+	public void onSave() {
+		AnimationData anim = Animations.getData(AnimationsTabController.instance.editing.id,
+				AnimationsTabController.instance.editing.group);
 		AnimationData previous = new AnimationData(anim);
 		anim = this.update(anim);
-		if (anim != null)
-		{
+		if (anim != null) {
 			this.updateVariants(anim, previous);
 			AnimationsTabController.instance.onEdited(anim);
 		}
 	}
 
-	public void onSpritesChange()
-	{
+	public void onSpritesChange() {
 		this.spritesTextfield.setDisable(!this.customSpritesRadio.isSelected());
 	}
 
-	public void onStateChange()
-	{
+	public void onStateChange() {
 		this.stateCombobox.setDisable(!this.stateCheckbox.isSelected());
 	}
 
-	protected void setupFor(AnimationData data)
-	{
+	protected void setupFor(AnimationData data) {
 		String apdelay = "";
-		for (int i = 0; i < data.alsoPlayDelay.length; ++i)
-		{
+		for (int i = 0; i < data.alsoPlayDelay.length; ++i) {
 			if (i != 0) apdelay += ",";
 			apdelay += data.alsoPlayDelay[i];
 		}
 		this.alsoplayDelayTextfield.setText(apdelay);
 		String ap = "";
-		for (int i = 0; i < data.alsoPlay.length; ++i)
-		{
+		for (int i = 0; i < data.alsoPlay.length; ++i) {
 			if (i != 0) ap += ",";
 			ap += data.alsoPlay[i];
 		}
@@ -241,11 +228,9 @@ public class EditAnimationController implements Initializable
 		this.delayTextfield.setText(String.valueOf(data.delayTime));
 		this.loopTextfield.setText(String.valueOf(data.loopsFrom));
 		if (data.spriteOrder == null) this.orderTextfield.setText("");
-		else
-		{
+		else {
 			String order = "";
-			for (int i = 0; i < data.spriteOrder.length; ++i)
-			{
+			for (int i = 0; i < data.spriteOrder.length; ++i) {
 				if (i != 0) order += ",";
 				order += data.spriteOrder[i];
 			}
@@ -266,60 +251,52 @@ public class EditAnimationController implements Initializable
 		this.heightTextfield.setText(String.valueOf(data.height));
 		this.xTextfield.setText(String.valueOf(data.gravityX));
 		this.yTextfield.setText(String.valueOf(data.gravityY));
+		this.overlayTextfield.setText(data.overlay == -1 ? "" : String.valueOf(data.overlay));
 
-		if (data.sprites == null)
-		{
+		if (data.sprites == null) {
 			this.noSpritesRadio.setSelected(true);
 			this.spritesTextfield.setText("");
-		} else if (data.sprites.equals("" + data.id))
-		{
+		} else if (data.sprites.equals("" + data.id)) {
 			this.defaultSpritesRadio.setSelected(true);
 			this.spritesTextfield.setText("");
-		} else
-		{
+		} else {
 			this.customSpritesRadio.setSelected(true);
 			this.spritesTextfield.setText(data.sprites);
 		}
 	}
 
-	public void setupFor(AnimationListItem item)
-	{
+	public void setupFor(AnimationListItem item) {
 		AnimationData data = Animations.getData(item.id, item.group);
 		this.setupFor(data);
 	}
 
-	protected AnimationData update(AnimationData data)
-	{
+	protected AnimationData update(AnimationData data) {
 		String in = this.alsoplayTextfield.getText();
-		if (!in.equals(""))
-		{
+		if (!in.equals("")) {
 			String[] ids = in.split(",");
 			data.alsoPlay = new String[ids.length];
 			System.arraycopy(ids, 0, data.alsoPlay, 0, ids.length);
 		} else data.alsoPlay = new String[0];
 
 		in = this.alsoplayDelayTextfield.getText();
-		if (in.equals(""))
-		{
+		if (in.equals("")) {
 			data.alsoPlayDelay = new int[data.alsoPlay.length];
 			for (int i = 0; i < data.alsoPlayDelay.length; ++i)
 				data.alsoPlayDelay[i] = 0;
-		} else
-		{
+		} else {
 			String[] delays = in.split(",");
 			data.alsoPlayDelay = new int[delays.length];
 			for (int i = 0; i < delays.length; ++i)
-				try
-				{
+				try {
 					data.alsoPlayDelay[i] = Integer.parseInt(delays[i]);
-				} catch (NumberFormatException e)
-				{
+				} catch (NumberFormatException e) {
 					FXUtils.showAlert("Wrong delay: " + delays[i]);
 					return null;
 				}
 		}
 
-		if (!this.animMovementCombobox.getValue().equals("none")) data.animationMovement = this.animMovementCombobox.getValue();
+		if (!this.animMovementCombobox.getValue().equals("none"))
+			data.animationMovement = this.animMovementCombobox.getValue();
 		else data.animationMovement = null;
 		data.backSpriteUsage = this.backspritesCombobox.getValue();
 		if (!this.clonesTextfield.getText().equals("")) data.clones = this.clonesTextfield.getText();
@@ -330,35 +307,37 @@ public class EditAnimationController implements Initializable
 		else data.loopsFrom = 0;
 
 		in = this.orderTextfield.getText();
-		if (!in.equals(""))
-		{
+		if (!in.equals("")) {
 			String[] indexes = in.split(",");
 			data.spriteOrder = new int[indexes.length];
 			for (int i = 0; i < indexes.length; ++i)
-				try
-				{
+				try {
 					data.spriteOrder[i] = Integer.parseInt(indexes[i]);
-				} catch (NumberFormatException e)
-				{
+				} catch (NumberFormatException e) {
 					FXUtils.showAlert("Wrong sprite index: " + indexes[i]);
 					return null;
 				}
 		} else data.spriteOrder = new int[0];
 
-		if (!this.overlayTextfield.getText().equals("")) data.overlay = Integer.parseInt(this.overlayTextfield.getText());
+		if (!this.overlayTextfield.getText().equals(""))
+			data.overlay = Integer.parseInt(this.overlayTextfield.getText());
 		else data.overlay = -1;
 		data.playsForEachTarget = this.playforeachtargetCheckbox.isSelected();
-		if (!this.pokemonMovementCombobox.getValue().equals("none")) data.pokemonMovement = this.pokemonMovementCombobox.getValue();
+		if (!this.pokemonMovementCombobox.getValue().equals("none"))
+			data.pokemonMovement = this.pokemonMovementCombobox.getValue();
 		else data.pokemonMovement = null;
-		if (!this.soundDelayTextfield.getText().equals("")) data.soundDelay = Integer.parseInt(this.soundDelayTextfield.getText());
+		if (!this.soundDelayTextfield.getText().equals(""))
+			data.soundDelay = Integer.parseInt(this.soundDelayTextfield.getText());
 		else data.soundDelay = 0;
 		if (!this.soundTextfield.getText().equals("")) data.sound = this.soundTextfield.getText();
 		else data.sound = null;
-		if (!this.spriteDurationTextfield.getText().equals("")) data.spriteDuration = Integer.parseInt(this.spriteDurationTextfield.getText());
+		if (!this.spriteDurationTextfield.getText().equals(""))
+			data.spriteDuration = Integer.parseInt(this.spriteDurationTextfield.getText());
 		else data.spriteDuration = 2;
 		if (this.stateCheckbox.isSelected()) data.pokemonState = this.stateCombobox.getValue();
 		else data.pokemonState = null;
-		if (!this.stateDelayTextfield.getText().equals("")) data.pokemonStateDelay = Integer.parseInt(this.stateDelayTextfield.getText());
+		if (!this.stateDelayTextfield.getText().equals(""))
+			data.pokemonStateDelay = Integer.parseInt(this.stateDelayTextfield.getText());
 		else data.pokemonStateDelay = 0;
 		if (!this.widthTextfield.getText().equals("")) data.width = Integer.parseInt(this.widthTextfield.getText());
 		else data.width = 0;
@@ -376,28 +355,32 @@ public class EditAnimationController implements Initializable
 		return data;
 	}
 
-	private void updateVariant(AnimationData anim, AnimationData previous, AnimationData variant)
-	{
+	private void updateVariant(AnimationData anim, AnimationData previous, AnimationData variant) {
 		if (Arrays.equals(variant.alsoPlay, previous.alsoPlay)) variant.alsoPlay = anim.alsoPlay;
 		if (Arrays.equals(variant.alsoPlayDelay, previous.alsoPlayDelay)) variant.alsoPlayDelay = anim.alsoPlayDelay;
 		if ((variant.animationMovement == null && previous.animationMovement == null)
 				|| (variant.animationMovement != null && variant.animationMovement.equals(previous.animationMovement)))
 			variant.animationMovement = anim.animationMovement;
 		if (variant.backSpriteUsage == previous.backSpriteUsage) variant.backSpriteUsage = anim.backSpriteUsage;
-		if ((variant.clones == null && previous.clones == null) || (variant.clones != null && variant.clones.equals(previous.clones)))
+		if ((variant.clones == null && previous.clones == null)
+				|| (variant.clones != null && variant.clones.equals(previous.clones)))
 			variant.clones = anim.clones;
 		if (variant.delayTime == previous.delayTime) variant.delayTime = anim.delayTime;
 		if (variant.loopsFrom == previous.loopsFrom) variant.loopsFrom = anim.loopsFrom;
 		if (Arrays.equals(variant.spriteOrder, previous.spriteOrder)) variant.spriteOrder = anim.spriteOrder;
 		if (variant.overlay == previous.overlay) variant.overlay = anim.overlay;
-		if (variant.playsForEachTarget == previous.playsForEachTarget) variant.playsForEachTarget = anim.playsForEachTarget;
+		if (variant.playsForEachTarget == previous.playsForEachTarget)
+			variant.playsForEachTarget = anim.playsForEachTarget;
 		if ((variant.pokemonMovement == null && previous.pokemonMovement == null)
 				|| (variant.pokemonMovement != null && variant.pokemonMovement.equals(previous.pokemonMovement)))
 			variant.pokemonMovement = anim.pokemonMovement;
-		if ((variant.sound == null && previous.sound == null) || (variant.sound != null && variant.sound.equals(previous.sound))) variant.sound = anim.sound;
+		if ((variant.sound == null && previous.sound == null)
+				|| (variant.sound != null && variant.sound.equals(previous.sound)))
+			variant.sound = anim.sound;
 		if (variant.soundDelay == previous.soundDelay) variant.soundDelay = anim.soundDelay;
 		if (variant.spriteDuration == previous.spriteDuration) variant.spriteDuration = anim.spriteDuration;
-		if ((variant.sprites == null && previous.sprites == null) || (variant.sprites != null && variant.sprites.equals(previous.sprites)))
+		if ((variant.sprites == null && previous.sprites == null)
+				|| (variant.sprites != null && variant.sprites.equals(previous.sprites)))
 			variant.sprites = anim.sprites;
 		if (variant.pokemonState == previous.pokemonState) variant.pokemonState = anim.pokemonState;
 		if (variant.pokemonStateDelay == previous.pokemonStateDelay) variant.pokemonStateDelay = anim.pokemonStateDelay;
@@ -407,8 +390,7 @@ public class EditAnimationController implements Initializable
 		if (variant.gravityY == previous.gravityY) variant.gravityY = anim.gravityY;
 	}
 
-	private void updateVariants(AnimationData anim, AnimationData previous)
-	{
+	private void updateVariants(AnimationData anim, AnimationData previous) {
 		for (int i = 0; i < previous.variants.length; ++i)
 			this.updateVariant(anim, previous, anim.variants[i]);
 	}
