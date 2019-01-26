df370798082cd0d74fe0d52ba63331aab9304de4
Shane Duan <s_duan@u.pacific.edu>
2019-01-26 11:34:26 -0800

    Convert AbstractGraphicLayer into an XMLObject

--- PMDMMOc/src/com/darkxell/client/renderers/layers/AbstractGraphicLayer.java
+++ PMDMMOc/src/com/darkxell/client/renderers/layers/AbstractGraphicLayer.java
@@ -1,16 +1,20 @@
 package com.darkxell.client.renderers.layers;
 
-import java.awt.Graphics2D;
+import com.darkxell.common.util.XMLObject;
+import org.jdom2.Element;
+
+import java.awt.*;
 
 /**
  * This object describes an animated background or foreground. This asset is
  * based on update/render mechanics. The render method must render on the whole
  * screen.
  */
-public abstract class AbstractGraphicLayer {
+public abstract class AbstractGraphicLayer extends XMLObject {
+    @Override
+    protected void onInitialize(Element el) {
+    }
 
-	public abstract void update();
-
-	public abstract void render(Graphics2D g, int width, int height);
-
+    public abstract void update();
+    public abstract void render(Graphics2D g, int width, int height);
 }
