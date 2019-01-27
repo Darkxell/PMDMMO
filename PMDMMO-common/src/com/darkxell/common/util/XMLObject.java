b481aa84fbb4b6d17ae5d95ab757e8247e5d9301
Shane Duan <s_duan@u.pacific.edu>
2019-01-26 19:26:24 -0800

    Remove no-op constructor for XMLObject
    forgot you could do that

--- PMDMMO-common/src/com/darkxell/common/util/XMLObject.java
+++ PMDMMO-common/src/com/darkxell/common/util/XMLObject.java
@@ -15,22 +15,16 @@ import java.io.InputStream;
  * centralized nature of XML object creation where there are more classes created than classes where initialization
  * takes place, it is probably worth the extra hassle. At least, that's what I tell myself so I can sleep with a
  * clean conscience.</p>
+ *
+ * <p>If there are some fields that require default values, it is probably a good idea to stick them in a
+ * pre-initialization block so that you don't have to call {@code super()} every single time, but if you want to
+ * annoy everyone it is also possible to put them in the constructor.</p>
  */
 public abstract class XMLObject {
     /**
      * Has the initialization method already been called?
      */
     protected boolean initialized;
-
-    /**
-     * No-op constructor. Initialize instead with {@link #initialize(Element)}.
-     *
-     * <p>If there are some fields that require default values, it is probably a good idea to stick them in a
-     * pre-initialization block so that you don't have to call {@code super()} every single time, but if you want to
-     * annoy everyone it is also possible to put them in the constructor.</p>
-     */
-    public XMLObject() {
-    }
 
     /**
      * Initialize object from an XML file stream.
