d7f8a3cf5afeb830f96387ea4438065f7077a4b1
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2019-02-01 22:28:14 +0100

    Move: 508- Spite

--- PMDMMO-common/src/com/darkxell/common/status/conditions/ForceLastMoveStatusCondition.java
+++ PMDMMO-common/src/com/darkxell/common/status/conditions/ForceLastMoveStatusCondition.java
@@ -1,12 +1,9 @@
 package com.darkxell.common.status.conditions;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 import com.darkxell.common.dungeon.floor.Floor;
-import com.darkxell.common.event.DungeonEvent;
-import com.darkxell.common.event.move.MoveSelectionEvent;
-import com.darkxell.common.event.turns.GameTurn;
+import com.darkxell.common.event.DungeonUtils;
 import com.darkxell.common.pokemon.DungeonPokemon;
 import com.darkxell.common.pokemon.LearnedMove;
 import com.darkxell.common.status.AppliedStatusCondition;
@@ -24,7 +21,7 @@ public class ForceLastMoveStatusCondition extends StatusCondition {
 	@Override
 	public Pair<Boolean, Message> affects(Floor floor, AppliedStatusCondition condition, DungeonPokemon pokemon) {
 
-		LearnedMove move = this.findLastMove(floor, pokemon);
+		LearnedMove move = DungeonUtils.findLastMove(floor, pokemon);
 
 		if (move == null) return new Pair<>(false,
 				new Message("status.not_affected").addReplacement("<pokemon>", pokemon.getNickname())); // Couldn't find a move to target
@@ -35,7 +32,7 @@ public class ForceLastMoveStatusCondition extends StatusCondition {
 	@Override
 	public AppliedStatusCondition create(Floor floor, DungeonPokemon target, Object source, Random random) {
 		AppliedStatusCondition c = super.create(floor, target, source, random);
-		LearnedMove m = this.findLastMove(floor, target);
+		LearnedMove m = DungeonUtils.findLastMove(floor, target);
 		if (m != null) c.addFlag("move:" + m.moveId());
 		return c;
 	}
@@ -52,27 +49,6 @@ public class ForceLastMoveStatusCondition extends StatusCondition {
 			if (moveid != null && moveid.matches("\\d+")) return move.moveId() != Integer.parseInt(moveid);
 		}
 		return super.preventsUsingMove(move, pokemon, floor);
-	}
-
-	private LearnedMove findLastMove(Floor floor, DungeonPokemon pokemon) {
-		ArrayList<GameTurn> turns = floor.dungeon.listTurns();
-		for (int i = turns.size() - 1; i >= 0; --i) {
-			GameTurn turn = turns.get(i);
-			DungeonEvent[] events = turn.events();
-			for (int e = events.length - 1; e >= 0; --e) {
-				DungeonEvent event = events[e];
-				if (event instanceof MoveSelectionEvent) {
-					MoveSelectionEvent moveEvent = (MoveSelectionEvent) event;
-					if (moveEvent.usedMove().user == pokemon) {
-						LearnedMove move = moveEvent.usedMove().move;
-						for (int m = 0; m < pokemon.moveCount(); ++m)
-							if (pokemon.move(m).moveId() == move.moveId()) return move;
-					}
-				}
-			}
-		}
-
-		return null;
 	}
 
 }
