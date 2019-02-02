c1194f97aec188838dc85320b71eeadd723e7bdc
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2019-02-02 13:46:17 +0100

    Mobr: 504- Curse

--- PMDMMO-common/src/com/darkxell/common/move/effects/BlowbackRandomEffect.java
+++ PMDMMO-common/src/com/darkxell/common/move/effects/BlowbackRandomEffect.java
@@ -2,7 +2,6 @@ package com.darkxell.common.move.effects;
 
 import com.darkxell.common.dungeon.floor.Floor;
 import com.darkxell.common.event.move.MoveSelectionEvent.MoveUse;
-import com.darkxell.common.move.BlowbackEffect;
 import com.darkxell.common.move.MoveEffectCalculator;
 import com.darkxell.common.move.MoveEvents;
 import com.darkxell.common.pokemon.DungeonPokemon;
