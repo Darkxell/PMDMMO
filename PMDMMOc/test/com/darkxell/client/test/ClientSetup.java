6d2da99b886aa130dfd8bc609a556592ae7eabdb
Shane Duan <s_duan@u.pacific.edu>
2019-02-18 20:28:24 -0800

    Revert client test loading code to old version

--- PMDMMOc/test/com/darkxell/client/test/ClientSetup.java
+++ PMDMMOc/test/com/darkxell/client/test/ClientSetup.java
@@ -1,8 +1,19 @@
 package com.darkxell.client.test;
 
+import com.darkxell.client.launchable.ClientSettings;
 import com.darkxell.client.launchable.Launcher;
 import com.darkxell.client.launchable.Persistence;
+import com.darkxell.client.mechanics.animation.Animations;
+import com.darkxell.client.resources.images.SpriteFactory;
+import com.darkxell.client.resources.images.pokemon.PokemonSpritesets;
+import com.darkxell.client.resources.music.SoundManager;
 import com.darkxell.client.state.mainstates.PrincipalMainState;
+import com.darkxell.client.ui.Frame;
+import com.darkxell.common.Registries;
+import com.darkxell.common.util.Logger;
+import com.darkxell.common.util.language.Localization;
+
+import static com.darkxell.client.launchable.render.RenderProfile.PROFILE_SYNCHRONIZED;
 
 public class ClientSetup {
     private static boolean initialized = false;
@@ -11,9 +22,23 @@ public class ClientSetup {
         if (initialized)
             return;
 
+        Launcher.isRunning = true;
+
+        Logger.load("CLIENT-TEST");
+        Localization.load(false);
+        Registries.load();
+        SpriteFactory.load();
+        ClientSettings.load();
+        PokemonSpritesets.loadData();
+        Animations.loadData();
+        Persistence.soundmanager = new SoundManager();
         Persistence.isUnitTesting = true;
-        Launcher.main(null);
+
+        Persistence.frame = new Frame();
+        Persistence.frame.canvas.requestFocus();
         Persistence.stateManager = new PrincipalMainState();
+
+        Launcher.setProcessingProfile(PROFILE_SYNCHRONIZED);
 
         initialized = true;
     }
