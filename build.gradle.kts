d1949a1b67318adae8be2ac50131a5f9bddb46d0
Shane Duan <s_duan@u.pacific.edu>
2019-02-22 10:47:45 -0800

    Add pmd plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     id("nebula.dependency-lock") version "7.3.0"
+
+    pmd
 }
 
 allprojects {
     repositories {
+        mavenCentral()
         jcenter()
         maven("https://jitpack.io")
     }
@@ -11,4 +14,13 @@ allprojects {
 
 subprojects {
     version = "0.1.0-alpha"
+
+    apply(plugin = "pmd")
+
+    tasks.withType<Pmd>().configureEach {
+        ignoreFailures = true
+        ruleSetFiles = rootProject.files("config/pmd.xml")
+        // ruleSets overrides ruleSetFiles if not empty. it's really stupid.
+        ruleSets = listOf()
+    }
 }
