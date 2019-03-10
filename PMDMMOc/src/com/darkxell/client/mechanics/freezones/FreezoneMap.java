68ae26f35800116047f62b96fec969e6e7b2d570
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2019-03-10 12:14:42 +0100

    Freezone data can include friend locations

--- PMDMMOc/src/com/darkxell/client/mechanics/freezones/FreezoneMap.java
+++ PMDMMOc/src/com/darkxell/client/mechanics/freezones/FreezoneMap.java
@@ -97,6 +97,8 @@ public abstract class FreezoneMap {
         this.tilesets.put(key, tileset);
         return tileset;
     }
+    
+    protected void loadAdditional(Element root) {}
 
     private void loadTiles(Element root) {
         List<Element> tileEls = root.getChild("tiles").getChildren();
@@ -141,6 +143,7 @@ public abstract class FreezoneMap {
         this.mapHeight = this.tagIntText(root, "height") / TILE_SIZE;
 
         this.loadTiles(root);
+        this.loadAdditional(root);
     }
 
     public void addEntity(FreezoneEntity entity) {
